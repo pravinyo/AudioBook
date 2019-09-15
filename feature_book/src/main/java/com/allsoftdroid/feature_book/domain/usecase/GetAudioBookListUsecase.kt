76669d8ad6d8d73e535d6292e69e94c9b21ef0c1
Pravinyo <pravinyo12@gmail.com>
2019-09-15 22:02:00 +0530

    access changed

--- feature_book/src/main/java/com/allsoftdroid/feature_book/domain/usecase/GetAudioBookListUsecase.kt
+++ feature_book/src/main/java/com/allsoftdroid/feature_book/domain/usecase/GetAudioBookListUsecase.kt
@@ -2,7 +2,7 @@ package com.allsoftdroid.feature_book.domain.usecase
 
 import com.allsoftdroid.feature_book.domain.repository.AudioBookRepository
 
-internal class GetAudioBookListUsecase(
+class GetAudioBookListUsecase(
     private val audioBookRep: AudioBookRepository
 ) {
     suspend fun execute() {
