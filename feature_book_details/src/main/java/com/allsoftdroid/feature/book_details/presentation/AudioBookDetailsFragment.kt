d55d37ac1ea3a1b5ec462d39e5a1ba247faae95e
Pravinyo <pravinyo12@gmail.com>
2019-09-29 17:19:15 +0530

    initial commit

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/presentation/AudioBookDetailsFragment.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/presentation/AudioBookDetailsFragment.kt
@@ -1,2 +1,29 @@
 package com.allsoftdroid.feature.book_details.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.allsoftdroid.common.base.fragment.BaseContainerFragment
+import com.allsoftdroid.feature.book_details.R
+import com.allsoftdroid.feature.book_details.databinding.FragmentAudiobookDetailsBinding
+
+
+class AudioBookDetailsFragment : BaseContainerFragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val databinding : FragmentAudiobookDetailsBinding = inflateLayout(inflater,R.layout.fragment_audiobook_details,container)
+
+        val argument = arguments?.getString("bookId")?:""
+
+        Toast.makeText(context,argument,Toast.LENGTH_SHORT).show()
+
+        return databinding.root
+    }
+}
