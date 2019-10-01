e135ed9399703e0191a20d8f7a7f20293ecdb8d3
Pravinyo <pravinyo12@gmail.com>
2019-10-01 22:52:23 +0530

    data structure for network meta data response

---
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/data/network/response/GetAudioBookMetadataResponse.kt
@@ -0,0 +1,7 @@
+package com.allsoftdroid.feature.book_details.data.network.response
+
+import com.allsoftdroid.feature.book_details.data.model.AudioBookMetadataResultDataModel
+
+internal data class GetAudioBookMetadataResponse(
+    val response: AudioBookMetadataResultDataModel
+)
