c4083e45e0547eebc29dc88391c1a23603d54e80
Pravinyo <pravinyo12@gmail.com>
2019-10-02 21:45:06 +0530

    metadata repository interface

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/repository/AudioBookMetadataRepository.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/repository/AudioBookMetadataRepository.kt
@@ -1,9 +1,10 @@
 package com.allsoftdroid.feature.book_details.domain.repository
 
 import androidx.lifecycle.LiveData
+import com.allsoftdroid.feature.book_details.domain.model.AudioBookMetadataDomainModel
 
 interface AudioBookMetadataRepository {
 
     suspend fun loadMetadataForBookId(bookId : String)
-    suspend fun getMetadata()
+    suspend fun getMetadata() : LiveData<AudioBookMetadataDomainModel>
 }
