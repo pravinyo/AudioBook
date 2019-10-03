289065eb306041bf025e67fef9668c59346d6449
Pravinyo <pravinyo12@gmail.com>
2019-10-03 22:40:44 +0530

    extension function added to get database model

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/data/model/AudioBookTrackDataModel.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/data/model/AudioBookTrackDataModel.kt
@@ -1,5 +1,7 @@
 package com.allsoftdroid.feature.book_details.data.model
 
+import com.allsoftdroid.database.metadataCacheDB.entity.DatabaseTrackEntity
+
 internal data class AudioBookTrackDataModel(
     val filename : String,
     val creator : String,
@@ -11,3 +13,15 @@ internal data class AudioBookTrackDataModel(
     val format: String,
     val size : String
 )
+
+internal fun AudioBookTrackDataModel.toDatabaseModel(id:String):DatabaseTrackEntity =
+    DatabaseTrackEntity(
+        track_id = "$id@$trackNumber@$format",
+        trackAlbum_id = id,
+        filename = filename?:"NULL",
+        trackTitle = trackTitle,
+        trackNumber = trackNumber,
+        length = length,
+        format = format,
+        size = size
+    )
