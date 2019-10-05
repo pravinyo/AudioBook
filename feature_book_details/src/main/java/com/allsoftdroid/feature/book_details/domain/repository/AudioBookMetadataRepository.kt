4282902101e42f57a26f691acddba51c0c3fcc1e
Pravinyo <pravinyo12@gmail.com>
2019-10-05 14:28:08 +0530

    track list loading added to contract

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/repository/AudioBookMetadataRepository.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/repository/AudioBookMetadataRepository.kt
@@ -2,9 +2,14 @@ package com.allsoftdroid.feature.book_details.domain.repository
 
 import androidx.lifecycle.LiveData
 import com.allsoftdroid.feature.book_details.domain.model.AudioBookMetadataDomainModel
+import com.allsoftdroid.feature.book_details.domain.model.AudioBookTrackDomainModel
 
 interface AudioBookMetadataRepository {
 
     suspend fun loadMetadata()
     fun getMetadata() : LiveData<AudioBookMetadataDomainModel>
+
+
+    suspend fun loadTrackListData()
+    fun  getTrackList() : LiveData<List<AudioBookTrackDomainModel>>
 }
