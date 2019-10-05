8b7c49935cef198434275f5752187e4fd5b0e16a
Pravinyo <pravinyo12@gmail.com>
2019-10-05 23:41:38 +0530

    click listener added

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/presentation/recyclerView/views/TrackItemViewHolder.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/presentation/recyclerView/views/TrackItemViewHolder.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.allsoftdroid.feature.book_details.databinding.BookMediaTrackItemBinding
 import com.allsoftdroid.feature.book_details.domain.model.AudioBookTrackDomainModel
+import com.allsoftdroid.feature.book_details.presentation.recyclerView.adapter.TrackItemClickedListener
 
 /**
  * ViewHolder Class for binding the data to the Layout and constructing layout for the display
@@ -12,8 +13,9 @@ import com.allsoftdroid.feature.book_details.domain.model.AudioBookTrackDomainMo
 class TrackItemViewHolder private constructor(private val binding : BookMediaTrackItemBinding) : RecyclerView.ViewHolder(binding.root){
 
     // bind the data to the view
-    fun bind(item: AudioBookTrackDomainModel){
+    fun bind(item: AudioBookTrackDomainModel,listener: TrackItemClickedListener){
         binding.track = item
+        binding.clickListener = listener
         binding.executePendingBindings()
     }
 
