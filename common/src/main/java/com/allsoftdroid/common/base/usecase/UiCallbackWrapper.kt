7ee9e18f34461479fca2042cee670f7068ee2bb2
Pravinyo <pravinyo12@gmail.com>
2019-10-21 16:24:49 +0530

    use case handler getInstance method moved to UseCaseHandler class

--- common/src/main/java/com/allsoftdroid/common/base/usecase/UiCallbackWrapper.kt
+++ common/src/main/java/com/allsoftdroid/common/base/usecase/UiCallbackWrapper.kt
@@ -6,25 +6,11 @@ class UiCallbackWrapper< V : BaseUseCase.ResponseValues>(
 
 
 
-    override fun onSuccess(response: V) {
+    override suspend fun onSuccess(response: V) {
         mUseCaseHandler.notifyResponse(response,callback)
     }
 
-    override fun onError(t: Throwable) {
+    override suspend fun onError(t: Throwable) {
         mUseCaseHandler.notifyError(callback,t)
-    }
-
-    companion object{
-
-        private var INSTANCE : UseCaseHandler ? = null
-
-        fun getInstance() : UseCaseHandler{
-
-            if(INSTANCE == null){
-                INSTANCE = UseCaseHandler(UseCaseThreadPoolScheduler())
-            }
-
-            return INSTANCE!!
-        }
     }
 }
