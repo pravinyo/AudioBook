20da5d985750f7f4fe79834688bb05a9ef79f557
Pravinyo <pravinyo12@gmail.com>
2019-10-21 16:27:03 +0530

    implementation changed using usecase class

--- feature_book/src/main/java/com/allsoftdroid/feature_book/domain/usecase/GetAudioBookListUsecase.kt
+++ feature_book/src/main/java/com/allsoftdroid/feature_book/domain/usecase/GetAudioBookListUsecase.kt
@@ -1,12 +1,38 @@
 package com.allsoftdroid.feature_book.domain.usecase
 
+import com.allsoftdroid.common.base.extension.Event
+import com.allsoftdroid.common.base.usecase.BaseUseCase
 import com.allsoftdroid.feature_book.domain.repository.AudioBookRepository
+import timber.log.Timber
 
-class GetAudioBookListUsecase(
-    private val audioBookRep: AudioBookRepository
-) {
+//class GetAudioBookListUsecase(
+//    private val audioBookRep: AudioBookRepository
+//) {
+//    suspend fun execute() = audioBookRep.searchAudioBooks(page = 1)
+//
+//    fun getAudioBook() = audioBookRep.getAudioBooks()
+//}
+//
 
-    suspend fun execute() = audioBookRep.searchAudioBooks()
 
-    suspend fun getAudioBook() = audioBookRep.getAudioBooks()
+class GetAudioBookListUsecase( private val audioBookRep: AudioBookRepository) :
+    BaseUseCase<GetAudioBookListUsecase.RequestValues,GetAudioBookListUsecase.ResponseValues>(){
+
+    override suspend fun executeUseCase(requestValues: RequestValues?) {
+
+        Timber.d("Request received data=${requestValues?.pageNumber?:-1}")
+        val pageNumber = requestValues?.pageNumber?:1
+
+
+        audioBookRep.searchAudioBooks(page = pageNumber)
+        Timber.d("fetching started")
+
+        val responseValues = ResponseValues(Event(Unit))
+        useCaseCallback?.onSuccess(responseValues)
+    }
+
+    fun getBookList() = audioBookRep.getAudioBooks()
+
+    class RequestValues(val pageNumber : Int) : BaseUseCase.RequestValues
+    class ResponseValues (val event : Event<Any>) : BaseUseCase.ResponseValues
 }
