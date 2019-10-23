aef6ea9cf05dac6e071de645d84b5f211b0c8684
Pravinyo <pravinyo12@gmail.com>
2019-10-23 21:48:31 +0530

    track list usecase implementation changed

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/usecase/GetTrackListUsecase.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/usecase/GetTrackListUsecase.kt
@@ -1,11 +1,23 @@
 package com.allsoftdroid.feature.book_details.domain.usecase
 
+import com.allsoftdroid.common.base.extension.Event
+import com.allsoftdroid.common.base.usecase.BaseUseCase
 import com.allsoftdroid.feature.book_details.domain.repository.AudioBookMetadataRepository
+import timber.log.Timber
 
 class GetTrackListUsecase(
-    private val metadataRepository: AudioBookMetadataRepository
-){
-    suspend fun loadTrackListData() = metadataRepository.loadTrackListData()
+    private val metadataRepository: AudioBookMetadataRepository):
+    BaseUseCase<GetTrackListUsecase.RequestValues,GetTrackListUsecase.ResponseValues>(){
+
+    override suspend fun executeUseCase(requestValues: RequestValues?) {
+        metadataRepository.loadTrackListData()
+        Timber.d("fetching started")
+
+        useCaseCallback?.onSuccess(ResponseValues(Event(Unit)))
+    }
 
     fun getTrackListData() = metadataRepository.getTrackList()
+
+    class RequestValues(val bookId : String) : BaseUseCase.RequestValues
+    class ResponseValues (val event : Event<Any>) : BaseUseCase.ResponseValues
 }
