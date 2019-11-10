15d83859353755c762756e5b7597b2ee0deee857
Pravinyo <pravinyo12@gmail.com>
2019-11-10 21:24:44 +0530

    initial, PlaySelectedTrack and TrackDetails event added

--- common/src/main/java/com/allsoftdroid/common/base/store/AudioPlayerEvent.kt
+++ common/src/main/java/com/allsoftdroid/common/base/store/AudioPlayerEvent.kt
@@ -1,8 +1,14 @@
 package com.allsoftdroid.common.base.store
 
+import com.allsoftdroid.common.base.extension.AudioPlayListItem
+
 sealed class AudioPlayerEvent
 
 data class Next(val result:Any) : AudioPlayerEvent()
 data class Previous(val result: Any) : AudioPlayerEvent()
 data class Play(val result: Any) : AudioPlayerEvent()
 data class Pause(val result: Any) : AudioPlayerEvent()
+data class Initial(val default: String):AudioPlayerEvent()
+
+data class PlaySelectedTrack(val trackList : List<AudioPlayListItem>,val bookId:String, val position:Int) : AudioPlayerEvent()
+data class TrackDetails(val trackTitle:String,val bookId: String):AudioPlayerEvent()
