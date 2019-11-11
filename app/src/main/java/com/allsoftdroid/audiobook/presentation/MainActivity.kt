570e4d3b337ed3eecec5362d79d48a4c9522a25d
Pravinyo <pravinyo12@gmail.com>
2019-11-11 17:35:30 +0530

    connection listener added

--- app/src/main/java/com/allsoftdroid/audiobook/presentation/MainActivity.kt
+++ app/src/main/java/com/allsoftdroid/audiobook/presentation/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.allsoftdroid.audiobook.presentation
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -10,15 +12,18 @@ import com.allsoftdroid.audiobook.presentation.viewModel.MainActivityViewModelFa
 import com.allsoftdroid.audiobook.services.audio.AudioManager
 import com.allsoftdroid.common.base.activity.BaseActivity
 import com.allsoftdroid.common.base.extension.Event
+import com.allsoftdroid.common.base.network.ConnectivityReceiver
 import com.allsoftdroid.common.base.store.*
 import com.allsoftdroid.common.base.utils.PlayerStatusListener
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 
 
-class MainActivity : BaseActivity(), PlayerStatusListener {
+class MainActivity : BaseActivity(), PlayerStatusListener,ConnectivityReceiver.ConnectivityReceiverListener {
 
     override val layoutResId = R.layout.activity_main
 
@@ -44,12 +49,23 @@ class MainActivity : BaseActivity(), PlayerStatusListener {
         AudioManager.getInstance(activity.applicationContext)
     }
 
+    private val snackbar by lazy {
+        val sb = Snackbar.make(findViewById(R.id.navHostFragment), "You are offline", Snackbar.LENGTH_LONG) //Assume "rootLayout" as the root layout of every activity.
+        sb.duration = BaseTransientBottomBar.LENGTH_INDEFINITE
+        sb
+    }
+
+    private val connectionListener by lazy {
+        ConnectivityReceiver()
+    }
+
     private lateinit var disposable : Disposable
 
     override fun onStart() {
         super.onStart()
 
         audioManager.bindAudioService()
+        registerReceiver(connectionListener, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
 
         Timber.d("Main Activity  start")
         mainActivityViewModel.showPlayer.observe(this, Observer {
@@ -132,9 +148,27 @@ class MainActivity : BaseActivity(), PlayerStatusListener {
         }
     }
 
+    override fun onNetworkConnectionChanged(isConnected: Boolean) {
+        showNetworkMessage(isConnected)
+    }
+
+    private fun showNetworkMessage(isConnected: Boolean) {
+        if (!isConnected) {
+            snackbar.show()
+        } else {
+            snackbar.dismiss()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ConnectivityReceiver.connectivityReceiverListener = this
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         disposable.dispose()
         audioManager.unBoundAudioService()
+        unregisterReceiver(connectionListener)
     }
 }
