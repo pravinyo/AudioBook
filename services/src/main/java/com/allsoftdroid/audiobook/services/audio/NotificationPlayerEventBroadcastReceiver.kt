18f64442aeddd23f3f3c4abaa7dd542991840eeb
Pravinyo <pravinyo12@gmail.com>
2019-11-16 22:19:18 +0530

    events type implementation added

--- services/src/main/java/com/allsoftdroid/audiobook/services/audio/NotificationPlayerEventBroadcastReceiver.kt
+++ services/src/main/java/com/allsoftdroid/audiobook/services/audio/NotificationPlayerEventBroadcastReceiver.kt
@@ -5,27 +5,40 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import com.allsoftdroid.common.base.extension.Event
+import com.allsoftdroid.common.base.store.*
 
 class NotificationPlayerEventBroadcastReceiver : BroadcastReceiver(){
+
+    private val eventStore : AudioPlayerEventStore by lazy {
+        AudioPlayerEventBus.getEventBusInstance()
+    }
+
     override fun onReceive(context: Context?, intent: Intent?) {
         context?.let {ctx ->
             intent?.let {
                 intent ->
 
+                //TODO: sometime it is playing and sometime it is not.
+                //TODO: On clicking on tracks it plays as expected
                 when(intent.action){
                     AudioService.NEXT -> {
+                        eventStore.publish(Event(Next("")))
                         Toast.makeText(ctx,AudioService.NEXT,Toast.LENGTH_SHORT).show()
                     }
 
                     AudioService.PREVIOUS -> {
+                        eventStore.publish(Event(Previous("")))
                         Toast.makeText(ctx,AudioService.PREVIOUS,Toast.LENGTH_SHORT).show()
                     }
 
                     AudioService.PLAY -> {
+                        eventStore.publish(Event(Play("")))
                         Toast.makeText(ctx,AudioService.PLAY,Toast.LENGTH_SHORT).show()
                     }
 
                     AudioService.PAUSE -> {
+                        eventStore.publish(Event(Pause("")))
                         Toast.makeText(ctx,AudioService.PAUSE,Toast.LENGTH_SHORT).show()
                     }
 
