cc3b199ba42b4362d6e4bdc69efa268263ebb802
Pravinyo <pravinyo12@gmail.com>
2019-11-18 17:15:28 +0530

    rx is removed

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/repository/BookDetailsSharedPreferenceRepository.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/repository/BookDetailsSharedPreferenceRepository.kt
@@ -1,19 +1,16 @@
 package com.allsoftdroid.feature.book_details.domain.repository
 
-import io.reactivex.Completable
-import io.reactivex.Observable
-
 /**
  * Interface for sharePreference for saving current state of the track Playing
  */
 
 interface BookDetailsSharedPreferenceRepository {
 
-    fun saveTrackPosition(pos : Int) : Completable
-    fun trackPosition() : Observable<Int>
+    fun saveTrackPosition(pos : Int)
+    fun trackPosition():Int
 
-    fun saveTrackTitle(title : String) : Completable
-    fun trackTitle() : Observable<String>
+    fun saveTrackTitle(title : String)
+    fun trackTitle():String
 
-    fun clear() : Completable
+    fun clear()
 }
