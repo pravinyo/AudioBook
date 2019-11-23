447d6c3e2f22cc9c26ff3c96418a5ca726fb2804
Pravinyo <pravinyo12@gmail.com>
2019-11-24 01:41:34 +0530

    media player is replaced with ExoPlayer

--- services/src/main/java/com/allsoftdroid/audiobook/services/audio/AudioServiceBinder.kt
+++ services/src/main/java/com/allsoftdroid/audiobook/services/audio/AudioServiceBinder.kt
@@ -2,36 +2,35 @@ package com.allsoftdroid.audiobook.services.audio
 
 import android.app.Application
 import android.content.Context
-import android.media.AudioManager
-import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Binder
-import android.os.PowerManager
-import android.text.TextUtils
+import androidx.core.net.toUri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.allsoftdroid.audiobook.services.R
 import com.allsoftdroid.common.base.extension.AudioPlayListItem
 import com.allsoftdroid.common.base.extension.Event
 import com.allsoftdroid.common.base.network.ArchiveUtils
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.Player.*
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import timber.log.Timber
 
 
-class AudioServiceBinder(application: Application) : Binder(),MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
-    MediaPlayer.OnCompletionListener {
+class AudioServiceBinder(application: Application) : Binder(){
 
-    // Check if stream audio.
-    private var streamAudio = true
+    private var exoPlayer: SimpleExoPlayer? = null
 
-    // Media player that play audio.
-    private var audioPlayer: MediaPlayer? = null
+    private var playerPrepared: Boolean = false
 
     // Caller activity context, used when play local audio file.
     private var context: Context = application
 
     //id of playing audio book
     private lateinit var bookId:String
-
-    private var isPlayerInitialized = false
 
     companion object {
         //song list
@@ -41,198 +40,190 @@ class AudioServiceBinder(application: Application) : Binder(),MediaPlayer.OnPrep
     //current position
     private var trackPos:Int = 0
 
+    /**
+     * EXO Code start
+     */
+
+    private val playerEventListener = object : EventListener {
+
+        override fun onPlayerError(error: ExoPlaybackException) {
+            Timber.w(error, "onPlayerError type: ${error.type}")
+        }
+
+        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            Timber.d("onPlayerStateChanged -> playWhenReady: $playWhenReady, playbackState: $playbackState, playbackError: ${exoPlayer?.playbackError}")
+
+            if (exoPlayer?.playbackError != null) {
+                Timber.d("Error detected ")
+
+                playerPrepared = false
+                return
+            }
+
+            if (shouldPreparePlayerAgain(playWhenReady, playbackState)) {
+                Timber.d("Preparing player again")
+                preparePlayer()
+            }
+
+            when(playbackState){
+                STATE_ENDED -> {
+                    Timber.d("ENDED")
+                }
+
+                STATE_IDLE -> Timber.d("IDLE")
+                STATE_BUFFERING -> Timber.d("Buffering")
+                STATE_READY -> Timber.d("Ready")
+            }
+        }
+
+        override fun onPositionDiscontinuity(reason: Int) {
+
+            exoPlayer?.let {
+                Timber.d("Event ended audio about to start new")
+                if(trackPos != it.currentWindowIndex){
+                    nextTrackEvent.value= Event(true)
+                    _trackTitle.value = trackList[it.currentWindowIndex].title
+                    trackPos = it.currentWindowIndex
+                }
+            }
+        }
+    }
+
+    private fun shouldPreparePlayerAgain(playWhenReady: Boolean, playbackState: Int) = playWhenReady && (playbackState == STATE_IDLE || playbackState == STATE_ENDED)
+
+    private fun preparePlayer() {
+        Timber.d("Preparing player")
+        playerPrepared = true
+        exoPlayer?.prepare(createMediaSource(trackList))
+        exoPlayer?.seekTo(trackPos,C.TIME_UNSET)
+    }
+
+    private fun createMediaSource(playlist : List<AudioPlayListItem>): ConcatenatingMediaSource {
+        Timber.d("Create media source called")
+        val dataSourceFactory = DefaultDataSourceFactory(context, Util.getUserAgent(context, context.getString(R.string.audio_player_service)))
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+
+        Timber.d("Building media list")
+        for (sample in playlist) {
+            val mediaSource = ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(ArchiveUtils.getRemoteFilePath(sample.filename,bookId).toUri())
+            concatenatingMediaSource.addMediaSource(mediaSource)
+        }
+
+        return concatenatingMediaSource
+    }
+
+    private fun onCreate(){
+        Timber.d("Created")
+        this.exoPlayer = ExoPlayerFactory.newSimpleInstance(context, DefaultTrackSelector()).apply {
+            addListener(playerEventListener)
+            Timber.d("Listener attached")
+        }
+        Timber.d("Player created")
+    }
+
+    fun onUnbind() {
+        Timber.i("Destroyed")
+        exoPlayer?.run {
+            removeListener(playerEventListener)
+            release()
+            Timber.d("Release player")
+        }
+
+        exoPlayer = null
+    }
+
+    fun goToPreviousOrBeginning() {
+        Timber.d("Previous called")
+        exoPlayer?.run {
+            if (hasPrevious()) {
+                previous()
+                _trackTitle.value = trackList[this.currentWindowIndex].title
+                Timber.d("Title: ${trackTitle.value}")
+            } else {
+                seekToDefaultPosition()
+                Timber.d("default Title: ${trackTitle.value}")
+            }
+        }
+    }
+
+    fun goToNext() {
+        Timber.d("Next called")
+        exoPlayer?.let {
+            if(it.hasNext()){
+                it.next()
+                _trackTitle.value = trackList[it.currentWindowIndex].title
+                Timber.d("Title: ${trackTitle.value}")
+            }else{
+                Timber.d("Track completed playing")
+            }
+        }
+    }
+
+    /**
+     * EXO code end
+     */
 
 
+    //TODO: Rx
     private var _trackTitle = MutableLiveData<String>()
     val trackTitle : LiveData<String>
         get() = _trackTitle
 
 
-    val nextTrack = MutableLiveData<Event<Boolean>>()
+    //TODO: Rx
+    val nextTrackEvent = MutableLiveData<Event<Boolean>>()
 
-    private fun getContext() = context
-
-    private fun isStreamAudio(): Boolean {
-        return streamAudio
-    }
-
-    fun isPlaying() = audioPlayer?.isPlaying?:false
-
-    fun setStreamAudio(streamAudio: Boolean) {
-        this.streamAudio = streamAudio
-    }
+    fun isPlaying() = exoPlayer?.isPlaying?:false
 
     private fun setTrackPosition(pos:Int){
         trackPos = pos
+
+        if (isPlaying()){
+            exoPlayer?.stop()
+            Timber.d("Player stopped")
+        }
+
+        exoPlayer?.let {
+            it.seekTo(pos, C.TIME_UNSET)
+            Timber.d("Seek is set to $pos")
+            it.playWhenReady = true
+            _trackTitle.value = trackList[it.currentWindowIndex].title
+            Timber.d("Track title updated and soon will be played")
+        }
     }
 
     fun setBookId(id:String){
         bookId = id
+        Timber.d("Book ID is $id")
     }
 
     fun setMultipleTracks(tracks: List<AudioPlayListItem>){
         trackList = tracks
+        Timber.d("Track list set and it's size is ${tracks.size}")
     }
 
-    fun getCurrentTrackTitle() = trackTitle.value
-
-
-    // Pause playing audio.
-    fun pauseAudio() {
-        if (audioPlayer != null) {
-            audioPlayer!!.pause()
-        }
-    }
-
-    fun continueAudio(){
-        if (audioPlayer != null) {
-            audioPlayer!!.start()
-        }
-    }
-
-    // Stop play audio.
-    fun stopAudio() {
-        if (audioPlayer != null) {
-            audioPlayer!!.stop()
-        }
-    }
-
-    fun initializeAndPlay(pos : Int=0){
-
-        if(!isPlayerInitialized){
-            initAudioPlayer()
-            isPlayerInitialized = true
-        }
-
-        if(isStreamAudio()){
-            stopAudio()
-        }
-
-//        setBookId(id)
-        setTrackPosition(pos)
-        playTrack()
-    }
-
-    private fun initAudioPlayer(){
-        audioPlayer = MediaPlayer()
-
-        audioPlayer?.let {
-
-            it.setWakeMode(getContext(),
-                PowerManager.PARTIAL_WAKE_LOCK)
-            it.setAudioStreamType(AudioManager.STREAM_MUSIC)
-            //set listeners
-            it.setOnPreparedListener(this)
-            it.setOnCompletionListener(this)
-            it.setOnErrorListener(this)
-        }
-    }
-
-    private fun playTrack(){
-
-        audioPlayer?.let {player ->
-
-            player.reset()
-            Timber.d("Player Reset done")
-
-            val track = trackList[trackPos]
-            _trackTitle.value = track.title?:"UNKNOWN"
-
-            val filePath = ArchiveUtils.getRemoteFilePath(track.filename,bookId)
-
-            if (!TextUtils.isEmpty(filePath)) {
-                if (isStreamAudio()) {
-                    player.setAudioStreamType(AudioManager.STREAM_MUSIC)
-                }
-                player.setDataSource(filePath)
-            } else {
-                player.setDataSource(getContext(), Uri.parse(filePath))
-            }
-
-            player.prepareAsync()
-            Timber.d("Prepare async called")
-        }?:Timber.d("Audio Player is Null: ${audioPlayer==null}")
-    }
-
-    override fun onPrepared(player: MediaPlayer?) {
-        player?.start()
-        Timber.d("player is ready, called start()")
-    }
-
-    override fun onError(player: MediaPlayer?, p1: Int, p2: Int): Boolean {
-        player?.reset()
-        Timber.d("Error occurres reset called with player is null :${player == null}")
-        return false
-    }
-
-    override fun onCompletion(player: MediaPlayer?) {
-        player?.let {
-            if (player.currentPosition>0){
-                nextTrack.value = Event(true)
-            }
-        }
-    }
-
-    //skip to previous track
-    fun playPrev() {
-        if (trackPos<=0) return
-        trackPos = (trackPos-1)% trackList.size
-        playTrack()
-    }
-
-    //skip to next
-    fun playNext() {
-        trackPos = (trackPos+1)% trackList.size
-        Timber.d("Next track pos is $trackPos of ${trackList.size}")
-        playTrack()
-    }
-
-
-    // Destroy audio player.
-    fun destroyAudioPlayer() {
-        if (audioPlayer != null) {
-            if (audioPlayer!!.isPlaying) {
-                audioPlayer!!.stop()
-            }
-
-            audioPlayer!!.release()
-
-            audioPlayer = null
-
-            isPlayerInitialized = false
-        }
-    }
+    fun getCurrentTrackTitle(): String = trackTitle.value?:"UNKNOWN"
 
     // Return current audio play position.
     fun getCurrentAudioPosition(): Int {
         var ret = 0
-        if (audioPlayer != null) {
-            ret = audioPlayer!!.currentPosition
-        }
-        return ret
-    }
 
-    // Return total audio file duration.
-    private fun getTotalAudioDuration(): Int {
-        var ret = 0
-        if (audioPlayer != null) {
-            ret = audioPlayer!!.duration
+        exoPlayer?.let {
+            ret = it.currentWindowIndex
         }
-
-        return ret
-    }
-
-    // Return current audio player progress value.
-    fun getAudioProgress(): Int {
-        var ret = 0
-        val currAudioPosition = getCurrentAudioPosition()
-        val totalAudioDuration = getTotalAudioDuration()
-        if (totalAudioDuration > 0) {
-            ret = currAudioPosition * 100 / totalAudioDuration
-        }
+        Timber.d("Current track pos is $ret")
         return ret
     }
 
     fun getBookId() = bookId
+
+    fun initializeAndPlay(currentPos: Int) {
+        if (exoPlayer == null){
+            Timber.d("Player is not yet created. starting to create")
+            onCreate()
+        }
+        setTrackPosition(currentPos)
+        Timber.d("Track pos is set.")
+    }
 
 }
