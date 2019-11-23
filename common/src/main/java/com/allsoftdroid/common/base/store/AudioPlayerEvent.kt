1e21555db02fafa4b9970389ca94c14bfc2b9896
Pravinyo <pravinyo12@gmail.com>
2019-11-24 01:36:30 +0530

    AudioPlayer state is made as input parameter for events

--- common/src/main/java/com/allsoftdroid/common/base/store/AudioPlayerEvent.kt
+++ common/src/main/java/com/allsoftdroid/common/base/store/AudioPlayerEvent.kt
@@ -1,14 +1,15 @@
 package com.allsoftdroid.common.base.store
 
 import com.allsoftdroid.common.base.extension.AudioPlayListItem
+import com.allsoftdroid.common.base.extension.AudioPlayerEventState
 
 sealed class AudioPlayerEvent
 
-data class Next(val result:Any) : AudioPlayerEvent()
-data class Previous(val result: Any) : AudioPlayerEvent()
-data class Play(val result: Any) : AudioPlayerEvent()
-data class Pause(val result: Any) : AudioPlayerEvent()
-data class Initial(val default: String):AudioPlayerEvent()
+data class Next(val result:AudioPlayerEventState) : AudioPlayerEvent()
+data class Previous(val result: AudioPlayerEventState) : AudioPlayerEvent()
+data class Play(val result: AudioPlayerEventState) : AudioPlayerEvent()
+data class Pause(val result: AudioPlayerEventState) : AudioPlayerEvent()
+data class Initial(val default: AudioPlayerEventState):AudioPlayerEvent()
 
 data class PlaySelectedTrack(val trackList : List<AudioPlayListItem>,val bookId:String, val position:Int) : AudioPlayerEvent()
-data class TrackDetails(val trackTitle:String,val bookId: String):AudioPlayerEvent()
+data class TrackDetails(val trackTitle:String,val bookId: String, val position: Int):AudioPlayerEvent()
