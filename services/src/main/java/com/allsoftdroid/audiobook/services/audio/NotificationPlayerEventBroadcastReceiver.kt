68177f40c00ef03bd51c38ef09511460fbf1de1c
Pravinyo <pravinyo12@gmail.com>
2019-11-24 01:43:28 +0530

    playing state added and newPendingIntent method signature changed

--- services/src/main/java/com/allsoftdroid/audiobook/services/audio/NotificationPlayerEventBroadcastReceiver.kt
+++ services/src/main/java/com/allsoftdroid/audiobook/services/audio/NotificationPlayerEventBroadcastReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import com.allsoftdroid.common.base.extension.Event
+import com.allsoftdroid.common.base.extension.PlayingState
 import com.allsoftdroid.common.base.store.*
 
 class NotificationPlayerEventBroadcastReceiver : BroadcastReceiver(){
@@ -19,26 +20,40 @@ class NotificationPlayerEventBroadcastReceiver : BroadcastReceiver(){
             intent?.let {
                 intent ->
 
-                //TODO: sometime it is playing and sometime it is not.
-                //TODO: On clicking on tracks it plays as expected
                 when(intent.action){
                     AudioService.NEXT -> {
-                        eventStore.publish(Event(Next("")))
+
+                        eventStore.publish(Event(Next(PlayingState(
+                            playingItemIndex = intent.getIntExtra(ACTION_NEXT_ITEM_INDEX,0),
+                            action_need = true
+                        ))))
+
                         Toast.makeText(ctx,AudioService.NEXT,Toast.LENGTH_SHORT).show()
                     }
 
                     AudioService.PREVIOUS -> {
-                        eventStore.publish(Event(Previous("")))
+                        eventStore.publish(Event(Previous(PlayingState(
+                            playingItemIndex = intent.getIntExtra(ACTION_PREVIOUS_ITEM_INDEX,0),
+                            action_need = true
+                        ))))
                         Toast.makeText(ctx,AudioService.PREVIOUS,Toast.LENGTH_SHORT).show()
                     }
 
                     AudioService.PLAY -> {
-                        eventStore.publish(Event(Play("")))
+                        eventStore.publish(Event(Play(PlayingState(
+                            playingItemIndex = intent.getIntExtra(ACTION_PLAY_ITEM_INDEX,0),
+                            action_need = true
+                        ))))
+
                         Toast.makeText(ctx,AudioService.PLAY,Toast.LENGTH_SHORT).show()
                     }
 
                     AudioService.PAUSE -> {
-                        eventStore.publish(Event(Pause("")))
+                        eventStore.publish(Event(Pause(PlayingState(
+                            playingItemIndex = intent.getIntExtra(ACTION_PAUSE_ITEM_INDEX,0),
+                            action_need = true
+                        ))))
+
                         Toast.makeText(ctx,AudioService.PAUSE,Toast.LENGTH_SHORT).show()
                     }
 
@@ -49,10 +64,16 @@ class NotificationPlayerEventBroadcastReceiver : BroadcastReceiver(){
     }
 
     companion object {
-        fun newPendingIntent(context: Context,action : String,requestCode:Int): PendingIntent {
+        fun newPendingIntent(context: Context,action : String,requestCode:Int,key:String,value:Int): PendingIntent {
             val intent = Intent(context, NotificationPlayerEventBroadcastReceiver::class.java)
             intent.action = action
+            intent.putExtra(key,value)
             return PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         }
+
+        const val ACTION_NEXT_ITEM_INDEX = "action_next_index"
+        const val ACTION_PREVIOUS_ITEM_INDEX = "action_previous_index"
+        const val ACTION_PLAY_ITEM_INDEX = "action_play_index"
+        const val ACTION_PAUSE_ITEM_INDEX = "action_pause_index"
     }
 }
