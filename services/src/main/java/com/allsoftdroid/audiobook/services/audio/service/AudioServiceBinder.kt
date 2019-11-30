405b4241ceb1cb2c8e6d6b529858fe8fcea67a75
Pravinyo <pravinyo12@gmail.com>
2019-11-30 19:39:52 +0530

    fix added for play when next or previous pressed in pause state

--- services/src/main/java/com/allsoftdroid/audiobook/services/audio/service/AudioServiceBinder.kt
+++ services/src/main/java/com/allsoftdroid/audiobook/services/audio/service/AudioServiceBinder.kt
@@ -145,6 +145,7 @@ class AudioServiceBinder(application: Application) : Binder(){
         Timber.d("Previous called")
         exoPlayer?.run {
             if (hasPrevious()) {
+                this.playWhenReady = true
                 previous()
                 if(this.currentWindowIndex>=0){
                     _trackTitle.value = trackList[this.currentWindowIndex].title
@@ -161,6 +162,7 @@ class AudioServiceBinder(application: Application) : Binder(){
         Timber.d("Next called")
         exoPlayer?.let {
             if(it.hasNext()){
+                it.playWhenReady = true
                 it.next()
                 if(it.currentWindowIndex< trackList.size)
                 {
