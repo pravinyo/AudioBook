b0efdf4ddc61774f57d1e6ba03c126d4761ae753
Pravinyo <pravinyo12@gmail.com>
2019-12-03 23:08:55 +0530

    added required implementation

--- feature_book/src/test/java/com/allsoftdroid/feature_book/presentation/common/FakeAudioBookRepository.kt
+++ feature_book/src/test/java/com/allsoftdroid/feature_book/presentation/common/FakeAudioBookRepository.kt
@@ -1,10 +1,19 @@
-package com.allsoftdroid.feature_book.presentation
+package com.allsoftdroid.feature_book.presentation.common
 
 import androidx.lifecycle.MutableLiveData
 import com.allsoftdroid.feature_book.domain.model.AudioBookDomainModel
 import com.allsoftdroid.feature_book.domain.repository.AudioBookRepository
+import com.allsoftdroid.feature_book.domain.repository.NetworkResponseListener
 
 class FakeAudioBookRepository : AudioBookRepository{
+    override fun registerNetworkResponse(listener: NetworkResponseListener) {
+
+    }
+
+    override fun unRegisterNetworkResponse() {
+
+    }
+
     private var audioBooks = MutableLiveData<List<AudioBookDomainModel>>()
 
     override suspend fun fetchBookList(page: Int) {
