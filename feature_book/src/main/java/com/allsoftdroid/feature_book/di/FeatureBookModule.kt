70c6a82bde1003196cd9eca071734a9ff8b394c7
Pravinyo <pravinyo12@gmail.com>
2019-12-05 20:32:54 +0530

    data module updated with retrofit and save in database

--- feature_book/src/main/java/com/allsoftdroid/feature_book/di/FeatureBookModule.kt
+++ feature_book/src/main/java/com/allsoftdroid/feature_book/di/FeatureBookModule.kt
@@ -1,7 +1,11 @@
 package com.allsoftdroid.feature_book.di
 
 import com.allsoftdroid.common.base.usecase.UseCaseHandler
+import com.allsoftdroid.database.bookListDB.AudioBookDao
 import com.allsoftdroid.database.common.AudioBookDatabase
+import com.allsoftdroid.database.common.SaveInDatabase
+import com.allsoftdroid.feature_book.data.databaseExtension.SaveBookListInDatabase
+import com.allsoftdroid.feature_book.data.network.service.ArchiveBooksApi
 import com.allsoftdroid.feature_book.data.repository.AudioBookRepositoryImpl
 import com.allsoftdroid.feature_book.domain.repository.AudioBookRepository
 import com.allsoftdroid.feature_book.domain.usecase.GetAudioBookListUsecase
@@ -25,7 +29,7 @@ object FeatureBookModule {
             bookListViewModelModule,
             usecaseModule,
             repositoryModule,
-            databaseModule,
+            dataModule,
             jobModule
         ))
     }
@@ -52,14 +56,22 @@ object FeatureBookModule {
 
     private val repositoryModule : Module = module {
         single {
-            AudioBookRepositoryImpl(get()) as AudioBookRepository
+            AudioBookRepositoryImpl(get(),get(),get(named(name = BOOK_LIST_DATABASE))) as AudioBookRepository
         }
     }
 
 
-    private val databaseModule : Module = module {
+    private val dataModule : Module = module {
         single {
             AudioBookDatabase.getDatabase(get()).audioBooksDao()
+        }
+
+        single {
+            ArchiveBooksApi.RETROFIT_SERVICE
+        }
+
+        single(named(name = BOOK_LIST_DATABASE)) {
+            SaveBookListInDatabase.setup(bookDao = get()) as SaveInDatabase<AudioBookDao,SaveBookListInDatabase>
         }
     }
 
@@ -76,4 +88,5 @@ object FeatureBookModule {
 
     const val SUPER_VISOR_JOB = "SuperVisorJob"
     const val VIEW_MODEL_SCOPE = "ViewModelScope"
+    private const val BOOK_LIST_DATABASE = "SaveBookListInDatabase"
 }
