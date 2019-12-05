aa4eaf8da6985fa36e58fccb314ec0e9418f2d79
Pravinyo <pravinyo12@gmail.com>
2019-12-05 19:22:06 +0530

    fake data source for database

---
+++ feature_book/src/test/java/com/allsoftdroid/feature_book/data/dataSource/FakeAudioDataSource.kt
@@ -0,0 +1,49 @@
+package com.allsoftdroid.feature_book.data.dataSource
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.allsoftdroid.database.bookListDB.AudioBookDao
+import com.allsoftdroid.database.bookListDB.DatabaseAudioBook
+
+class FakeAudioDataSource(var bookList: MutableList<DatabaseAudioBook> = mutableListOf()) : AudioBookDao {
+
+    private var _bookListLiveData = MutableLiveData<List<DatabaseAudioBook>>()
+
+    override fun getBooks(): LiveData<List<DatabaseAudioBook>> {
+        _bookListLiveData.value = bookList
+        return _bookListLiveData
+    }
+
+    override fun getBookBy(identifier: String): DatabaseAudioBook {
+        bookList.forEach {
+            if(it.identifier == identifier) return it
+        }
+
+        return DatabaseAudioBook(identifier="",title = "",creator = "",date = "")
+    }
+
+    override fun insert(book: DatabaseAudioBook) {
+        bookList.add(book)
+    }
+
+    override fun insertAllList(books: List<DatabaseAudioBook>) {
+        bookList.addAll(books)
+    }
+
+    override fun deleteAll(): Int {
+        val size = bookList.size
+        bookList.clear()
+
+        return size
+    }
+
+    override fun deleteItem(identifier: String) {
+        bookList = bookList.filter {
+            it.identifier != identifier
+        }.toMutableList()
+    }
+
+    override fun getLastBook(): DatabaseAudioBook {
+        return bookList[bookList.lastIndex]
+    }
+}
