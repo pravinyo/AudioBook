23485695806a8349d6520303a3172131d44d729f
Pravinyo <pravinyo12@gmail.com>
2019-12-07 12:44:25 +0530

    AudioBookListFragment instrumentation test added

---
+++ feature_book/src/androidTest/java/com/allsoftdroid/feature_book/presentation/AudioBookListFragmentTest.kt
@@ -0,0 +1,20 @@
+package com.allsoftdroid.feature_book.presentation
+
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.allsoftdroid.feature_book.R
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class AudioBookListFragmentTest{
+
+
+    @Test
+    fun audioBookList_DisplayedInUI(){
+        launchFragmentInContainer<AudioBookListFragment>(themeResId = R.style.AppTheme)
+    }
+}
