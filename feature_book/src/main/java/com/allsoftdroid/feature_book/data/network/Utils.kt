8e0450cd644451b30568fe023f5c095f3f7b052b
Pravinyo <pravinyo12@gmail.com>
2019-12-29 20:45:52 +0530

    book search url path added

--- feature_book/src/main/java/com/allsoftdroid/feature_book/data/network/Utils.kt
+++ feature_book/src/main/java/com/allsoftdroid/feature_book/data/network/Utils.kt
@@ -9,10 +9,14 @@ class Utils {
 
         const val QUERY_PAGE = "page"
         const val QUERY_ROW = "rows"
+        const val QUERY_SEARCH="q"
 
         const val DEFAULT_ROW_COUNT = 50
         fun getBaseURL() = BASE_URL
 
         const val BOOKS_COLLECTION_PATH = "/advancedsearch.php?q=($QUERY)+AND+collection%3A($COLLECTIONS)$FILTER_MOST_RECENT"
+
+        const val BOOKS_SEARCH_PATH = "/advancedsearch.php?collection%3A($COLLECTIONS)$FILTER_MOST_RECENT"
+
     }
 }
