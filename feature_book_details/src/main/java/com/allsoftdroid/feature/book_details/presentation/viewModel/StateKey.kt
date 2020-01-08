dbbc063926dcaad4e49c7d9fadd614cb95a957b8
Pravinyo <pravinyo12@gmail.com>
2020-01-08 17:00:56 +0530

    enum class added for handling keys for state

---
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/presentation/viewModel/StateKey.kt
@@ -0,0 +1,6 @@
+package com.allsoftdroid.feature.book_details.presentation.viewModel
+
+enum class StateKey(var key:String) {
+    CurrentPlayingTrack("CurrentPlayingTrack"),
+    CurrentTrackFormat("CurrentTrackFormat")
+}
