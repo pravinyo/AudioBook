a2190e454fd041e933eea65f6aa06af2885dda59
Pravinyo <pravinyo12@gmail.com>
2020-01-17 12:59:29 +0530

    implemented new contract change

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/data/repository/BookDetailsSharedPreferencesRepositoryImpl.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/data/repository/BookDetailsSharedPreferencesRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.allsoftdroid.feature.book_details.data.repository
 
+import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
 import com.allsoftdroid.feature.book_details.domain.repository.BookDetailsSharedPreferenceRepository
@@ -12,7 +13,7 @@ class BookDetailsSharedPreferencesRepositoryImpl(private val preferences : Share
 
     companion object {
         @JvmStatic
-        fun create(context: Context): BookDetailsSharedPreferencesRepositoryImpl {
+        fun create(context: Application): BookDetailsSharedPreferencesRepositoryImpl {
             val preferences = context.getSharedPreferences("BookDetailsRxPrefs", Context.MODE_PRIVATE)
             return BookDetailsSharedPreferencesRepositoryImpl(preferences)
         }
@@ -20,6 +21,8 @@ class BookDetailsSharedPreferencesRepositoryImpl(private val preferences : Share
         private const val KEY_NAME_TRACK_PLAYING_NUMBER = "key_name_current_track_playing_number"
         private const val KEY_NAME_TRACK_PLAYING_TITLE = "key_name_current_track_playing_title"
         private const val KEY_NAME_TRACK_IS_PLAYING = "Key_name_is_track_playing"
+        private const val KEY_NAME_BOOK_ID="key_name_book_id"
+        private const val KEY_NAME_TRACK_FORMAT = "key_name_track_format"
     }
 
     override fun saveTrackPosition(pos: Int)=
@@ -29,12 +32,12 @@ class BookDetailsSharedPreferencesRepositoryImpl(private val preferences : Share
 
     override fun trackPosition():Int = preferences.getInt(KEY_NAME_TRACK_PLAYING_NUMBER,0)
 
-    fun saveIsPlaying(isPlaying: Boolean)=
+    override fun saveIsPlaying(isPlaying: Boolean)=
         preferences.editSharedPreferences {
             putBoolean(KEY_NAME_TRACK_IS_PLAYING,isPlaying)
         }
 
-    fun isPlaying():Boolean = preferences.getBoolean(KEY_NAME_TRACK_IS_PLAYING,false)
+    override fun isPlaying():Boolean = preferences.getBoolean(KEY_NAME_TRACK_IS_PLAYING,false)
 
     override fun saveTrackTitle(title: String) =
         preferences.editSharedPreferences {
@@ -43,10 +46,28 @@ class BookDetailsSharedPreferencesRepositoryImpl(private val preferences : Share
 
     override fun trackTitle():String = preferences.getString(KEY_NAME_TRACK_PLAYING_TITLE,"")?:""
 
+    override fun saveBookId(bookId: String) {
+        preferences.editSharedPreferences {
+            putString(KEY_NAME_BOOK_ID,bookId)
+        }
+    }
+
+    override fun bookId(): String = preferences.getString(KEY_NAME_BOOK_ID,"")?:""
+
+    override fun saveTrackFormatIndex(formatIndex: Int) {
+        preferences.editSharedPreferences {
+            putInt(KEY_NAME_TRACK_FORMAT,formatIndex)
+        }
+    }
+
+    override fun trackFormatIndex(): Int = preferences.getInt(KEY_NAME_TRACK_FORMAT,0)
+
     override fun clear() = preferences.clearSharedPreferences {
         remove(KEY_NAME_TRACK_PLAYING_TITLE)
         remove(KEY_NAME_TRACK_PLAYING_NUMBER)
         remove(KEY_NAME_TRACK_IS_PLAYING)
+        remove(KEY_NAME_BOOK_ID)
+        remove(KEY_NAME_TRACK_FORMAT)
     }
 
     private fun SharedPreferences.editSharedPreferences(batch: SharedPreferences.Editor.() -> Unit) = edit().also(batch).apply()
