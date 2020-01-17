c4865a6df428a4b2c674a7d65062dcd4ed710000
Pravinyo <pravinyo12@gmail.com>
2020-01-17 14:40:41 +0530

    last played usecase added

---
+++ app/src/main/java/com/allsoftdroid/audiobook/domain/usecase/lastPlayedUsecase.kt
@@ -0,0 +1,30 @@
+package com.allsoftdroid.audiobook.domain.usecase
+
+import com.allsoftdroid.audiobook.domain.model.LastPlayedTrack
+import com.allsoftdroid.common.base.usecase.BaseUseCase
+import com.allsoftdroid.feature.book_details.domain.repository.BookDetailsSharedPreferenceRepository
+
+class GetLastPlayedUsecase : BaseUseCase<GetLastPlayedUsecase.RequestValues, GetLastPlayedUsecase.ResponseValues>() {
+
+    override suspend fun executeUseCase(requestValues: RequestValues?) {
+        requestValues?.let { requestValues ->
+            requestValues.sharedPref.let {
+                if(it.bookId().isNotEmpty()){
+                    useCaseCallback?.onSuccess(
+                        ResponseValues(
+                            lastPlayedTrack = LastPlayedTrack(
+                                title = it.trackTitle(),
+                                position = it.trackPosition(),
+                                bookId = it.bookId(),
+                                formatIndex = it.trackFormatIndex()
+                            )
+                        )
+                    )
+                }
+            }
+        }?:useCaseCallback?.onError(Error("Request cannot be Null"))
+    }
+
+    class RequestValues(val sharedPref: BookDetailsSharedPreferenceRepository) : BaseUseCase.RequestValues
+    class ResponseValues (val lastPlayedTrack : LastPlayedTrack) : BaseUseCase.ResponseValues
+}
