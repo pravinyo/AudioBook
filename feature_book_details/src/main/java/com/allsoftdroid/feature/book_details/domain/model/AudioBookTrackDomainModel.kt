7c912e0786a78dbf92e29c2292e39dc290adb441
Pravinyo <pravinyo12@gmail.com>
2020-01-26 12:21:44 +0530

    download status field added

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/model/AudioBookTrackDomainModel.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/model/AudioBookTrackDomainModel.kt
@@ -1,8 +1,10 @@
 package com.allsoftdroid.feature.book_details.domain.model
 
 import com.allsoftdroid.common.base.extension.AudioPlayListItem
+import com.allsoftdroid.feature.book_details.utils.DownloadStatusEvent
 
 data class AudioBookTrackDomainModel(
+    var downloadStatus:DownloadStatusEvent = DownloadStatusEvent.NOTHING,
     var isPlaying:Boolean = false,
     override val filename : String,
     override val title : String?,
