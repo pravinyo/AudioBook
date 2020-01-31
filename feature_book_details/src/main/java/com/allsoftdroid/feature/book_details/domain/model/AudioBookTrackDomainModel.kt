5e2379a0706ed67ab6f1a3331d5652f62b020586
Pravinyo <pravinyo12@gmail.com>
2020-01-31 06:14:59 +0530

    enum changed to sealed class and progress event added

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/model/AudioBookTrackDomainModel.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/model/AudioBookTrackDomainModel.kt
@@ -2,9 +2,10 @@ package com.allsoftdroid.feature.book_details.domain.model
 
 import com.allsoftdroid.common.base.extension.AudioPlayListItem
 import com.allsoftdroid.feature.book_details.utils.DownloadStatusEvent
+import com.allsoftdroid.feature.book_details.utils.NOTHING
 
 data class AudioBookTrackDomainModel(
-    var downloadStatus:DownloadStatusEvent = DownloadStatusEvent.NOTHING,
+    var downloadStatus:DownloadStatusEvent = NOTHING,
     var isPlaying:Boolean = false,
     override val filename : String,
     override val title : String?,
