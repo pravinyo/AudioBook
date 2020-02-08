c7d3b7295aa1889b839a9c57571566866240ec3b
Pravinyo <pravinyo12@gmail.com>
2020-02-08 14:00:16 +0530

    core downloader interface

---
+++ feature_downloader/src/main/java/com/allsoftdroid/audiobook/feature_downloader/domain/IDownloaderCore.java
@@ -0,0 +1,8 @@
+package com.allsoftdroid.audiobook.feature_downloader.domain;
+
+import com.allsoftdroid.common.base.store.downloader.DownloadEvent;
+
+public interface IDownloaderCore {
+
+    void handleDownloadEvent(DownloadEvent downloadEvent);
+}
