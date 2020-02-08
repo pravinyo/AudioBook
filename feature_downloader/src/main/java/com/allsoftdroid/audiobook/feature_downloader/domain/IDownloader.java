a4ffcf9d073e0e53f18a9ac7cb6143bb6e3d0b03
Pravinyo <pravinyo12@gmail.com>
2020-02-08 14:00:37 +0530

    main downloader interface

---
+++ feature_downloader/src/main/java/com/allsoftdroid/audiobook/feature_downloader/domain/IDownloader.java
@@ -0,0 +1,26 @@
+package com.allsoftdroid.audiobook.feature_downloader.domain;
+
+import android.content.Context;
+
+public interface IDownloader extends IDownloaderCore {
+
+    void updateDownloaded(String mUrl,String mBookId,int mChapterIndex);
+    void updateProgress(String mUrl,String mBookId,int mChapterIndex,long progress);
+
+    long[] getProgress(long downloadId);
+    void bulkDownload(String[] urls, String[] names, String subPath, String title);
+
+    void removeFromDownloadDatabase(long downloadId);
+
+    void LogAllLocalData();
+    void clearAllDownloadedEntry();
+
+    String findURLbyDownloadId(long downloadId);
+    long getDownloadIdByURL(String url);
+
+    String[] getStatusByDownloadId(long downloadId);
+
+    void openDownloadedFile(Context context, long downloadId);
+
+    void Destroy();
+}
