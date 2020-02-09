3a7e436303054f192dd8f4c5a07a2af0a7eaac6b
Pravinyo <pravinyo12@gmail.com>
2020-02-09 21:56:37 +0530

    http logging added and default values added in searchBookInRemoteRepository method

--- feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/network/LibriVoxApiService.kt
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/network/LibriVoxApiService.kt
@@ -1,5 +1,7 @@
 package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -8,12 +10,21 @@ import retrofit2.http.Headers
 import retrofit2.http.Query
 
 
+private fun getHttpClientListener(): OkHttpClient {
+    val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+
+    return OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+}
+
 /**
  * create retrofit instance
  */
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(Utils.Books.getBaseURL())
+    .client(getHttpClientListener())
     .build()
 
 
@@ -23,25 +34,25 @@ private val retrofit = Retrofit.Builder()
  */
 interface LibriVoxApiService{
 
-    @Headers("X-Requested-With:XMLHttpRequest")
-    @GET(Utils.Books.LIBRIVOX)
-    fun getBooks():Call<String>
+//    @Headers("X-Requested-With:XMLHttpRequest")
+//    @GET(Utils.Books.LIBRIVOX)
+//    fun getBooks():Call<String>
 
     @Headers("X-Requested-With:XMLHttpRequest")
-    @GET(Utils.Books.LIBRIVOX)
+    @GET(Utils.Books.ADVANCED_SEARCH)
     fun searchBookInRemoteRepository(
         @Query("title") title:String,
-        @Query("author") author:String,
-        @Query("reader") reader:String,
-        @Query("keywords") keywords:String,
-        @Query("genre_id") genre_id:String,
-        @Query("status") status:String,
-        @Query("project_type") project_type:String,
-        @Query("recorded_language") recorded_language:String,
-        @Query("sort_order") sort_order:String,
-        @Query("search_page") search_page:String,
-        @Query("search_form") search_form:String,
-        @Query("q") q:String):Call<String>
+        @Query("author") author:String="",
+        @Query("reader") reader:String="",
+        @Query("keywords") keywords:String="",
+        @Query("genre_id") genre_id:Int=0,
+        @Query("status") status:String="all",
+        @Query("project_type") project_type:String="either",
+        @Query("recorded_language") recorded_language:String="",
+        @Query("sort_order") sort_order:String="catalog_date",
+        @Query("search_page") search_page:Int=1,
+        @Query("search_form") search_form:String="advanced",
+        @Query("q") q:String=""):Call<String>
 }
 
 
