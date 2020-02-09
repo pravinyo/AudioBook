dbda2929d1f67db92ffdb21546016119a5d65a84
Pravinyo <pravinyo12@gmail.com>
2020-02-09 22:00:00 +0530

    method params provided, network state added

--- feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/repository/EnhanceDetailsRepositoryImpl.kt
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/repository/EnhanceDetailsRepositoryImpl.kt
@@ -7,6 +7,10 @@ import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.network.LibriVoxApi
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.network.NetworkResponse
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain.EnhanceDetailsRepository
+import com.allsoftdroid.common.base.extension.Event
+import com.allsoftdroid.common.base.network.Failure
+import com.allsoftdroid.common.base.network.NetworkResult
+import com.allsoftdroid.common.base.network.Success
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -14,6 +18,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import timber.log.Timber
+import java.lang.Error
 
 class EnhanceDetailsRepositoryImpl : EnhanceDetailsRepository {
 
@@ -34,21 +39,25 @@ class EnhanceDetailsRepositoryImpl : EnhanceDetailsRepository {
     /***
      * track network repsonse for  completion and started
      */
-    private var _response = MutableLiveData<Int>()
-    val response: LiveData<Int>
+    private var _response = MutableLiveData<Event<NetworkResult>>()
+    val response: LiveData<Event<NetworkResult>>
         get() = _response
 
 
-    override suspend fun searchBookDetails(){
+    override suspend fun searchBookDetailsInRemoteRepository(searchTitle:String,author:String,page:Int){
         Timber.d("find enhance book details called")
 
         _response.value = null
         withContext(Dispatchers.IO){
             Timber.d("Starting network call")
-            LibriVoxApi.retrofitService.getBooks().enqueue(object : Callback<String> {
+            LibriVoxApi.retrofitService.searchBookInRemoteRepository(
+                title = searchTitle,
+                author = author,
+                search_page = page
+            ).enqueue(object : Callback<String> {
                 override fun onFailure(call: Call<String>, t: Throwable) {
                     Timber.d("Failure occur")
-                    _response.value = null
+                    _response.value = Event(Failure(Error(t.message)))
                 }
 
                 override fun onResponse(call: Call<String>, response: Response<String>) {
@@ -56,10 +65,10 @@ class EnhanceDetailsRepositoryImpl : EnhanceDetailsRepository {
                     val result = gson.fromJson(response.body(), NetworkResponse::class.java)
                     Timber.d("Response got:${response.body()}")
                     result?.results?.let {
-                        _response.value = result.status.length
+                        _response.value = Event(Success(true))
                         _bookSearchResult.value = result.results
-                        _listOfItem.value = BookBestMatchFromNetworkResultRepository.getList(it)
-                        bestMatch = BookBestMatchFromNetworkResultRepository.rank
+                        _listOfItem.value = BookBestMatchFromNetworkResult.getList(it)
+                        bestMatch = BookBestMatchFromNetworkResult.rank
                     }
                 }
             })
@@ -68,7 +77,7 @@ class EnhanceDetailsRepositoryImpl : EnhanceDetailsRepository {
 
     override suspend fun fetchBookDetails(bookUrl:String){
         withContext(Dispatchers.IO){
-            val details = BookDetailsParsingFromNetworkResponseRespository.loadDetails(bookUrl)
+            val details = BookDetailsParsingFromNetworkResponse.loadDetails(bookUrl)
             withContext(Dispatchers.Main){
                 _bookDetails.value = details
             }
