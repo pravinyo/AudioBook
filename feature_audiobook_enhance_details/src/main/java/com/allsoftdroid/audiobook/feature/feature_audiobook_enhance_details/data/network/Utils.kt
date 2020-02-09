44f74fff7786b5c04bea630b513cc572ecef80ff
Pravinyo <pravinyo12@gmail.com>
2020-02-09 21:57:15 +0530

    const added for advanced search path

--- feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/network/Utils.kt
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/network/Utils.kt
@@ -7,5 +7,6 @@ class Utils{
         //TODO: Need changes here
         const val param = "title=Poems&author=&reader=&keywords=&genre_id=0&status=all&project_type=either&recorded_language=&sort_order=catalog_date&search_page=1&search_form=advanced&q="
         const val LIBRIVOX = "/advanced_search?$param"
+        const val ADVANCED_SEARCH="/advanced_search"
     }
 }
