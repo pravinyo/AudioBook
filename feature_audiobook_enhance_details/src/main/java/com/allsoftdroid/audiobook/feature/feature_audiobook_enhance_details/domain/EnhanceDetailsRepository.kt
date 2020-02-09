231868e513151d024c21fb288a644b1c911949e7
Pravinyo <pravinyo12@gmail.com>
2020-02-09 22:00:37 +0530

    renamed to meaningful name

--- feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/domain/EnhanceDetailsRepository.kt
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/domain/EnhanceDetailsRepository.kt
@@ -1,6 +1,6 @@
 package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain
 
 interface EnhanceDetailsRepository {
-    suspend fun searchBookDetails()
+    suspend fun searchBookDetailsInRemoteRepository(searchTitle:String,author:String,page:Int)
     suspend fun fetchBookDetails(bookUrl:String)
 }
