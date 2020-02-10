906e7e02390b358658fbefbffb1f37e294353a2b
Pravinyo <pravinyo12@gmail.com>
2020-02-10 22:04:25 +0530

    additional methods added to contract

--- feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/domain/EnhanceDetailsRepository.kt
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/domain/EnhanceDetailsRepository.kt
@@ -1,6 +1,16 @@
 package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain
 
+import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.BookDetails
+import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.WebDocument
+import com.allsoftdroid.common.base.network.NetworkResult
+
 interface EnhanceDetailsRepository {
     suspend fun searchBookDetailsInRemoteRepository(searchTitle:String,author:String,page:Int)
+    fun getSearchBooksList(): List<WebDocument>
+    fun getBookListWithRanks(): List<Pair<Int, WebDocument>>
+
+    fun networkStatus() : NetworkResult
+
     suspend fun fetchBookDetails(bookUrl:String)
+    fun getBookDetails():BookDetails
 }
