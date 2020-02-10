56b7e0865e9702ddc7e26e52005501dc9c91ec3d
Pravinyo <pravinyo12@gmail.com>
2020-02-10 22:04:54 +0530

    additional methods implemented

--- feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/repository/EnhanceDetailsRepositoryImpl.kt
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/repository/EnhanceDetailsRepositoryImpl.kt
@@ -1,14 +1,12 @@
 package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.BookDetails
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.WebDocument
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.network.LibriVoxApi
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.network.NetworkResponse
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain.EnhanceDetailsRepository
-import com.allsoftdroid.common.base.extension.Event
 import com.allsoftdroid.common.base.network.Failure
+import com.allsoftdroid.common.base.network.Loading
 import com.allsoftdroid.common.base.network.NetworkResult
 import com.allsoftdroid.common.base.network.Success
 import com.google.gson.Gson
@@ -18,36 +16,29 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import timber.log.Timber
-import java.lang.Error
 
 class EnhanceDetailsRepositoryImpl : EnhanceDetailsRepository {
 
     /***
      * hold stories related to politics
      */
-    private var _bookSearchResult = MutableLiveData<String>()
-    val bookSearchResult:LiveData<String> = _bookSearchResult
+    private lateinit var _bookSearchResult:String
 
-    private var _listOfItem = MutableLiveData<List<WebDocument>>()
-    val listOfItem : LiveData<List<WebDocument>> = _listOfItem
+    private lateinit var _listOfItem :List<WebDocument>
 
-    private var _bookDetails = MutableLiveData<BookDetails>()
-    val bookDetails:LiveData<BookDetails> = _bookDetails
+    private lateinit var _bookDetails:BookDetails
 
-    lateinit var bestMatch: List<Pair<Int, WebDocument>>
+    lateinit var _bestMatch: List<Pair<Int, WebDocument>>
 
     /***
      * track network repsonse for  completion and started
      */
-    private var _response = MutableLiveData<Event<NetworkResult>>()
-    val response: LiveData<Event<NetworkResult>>
-        get() = _response
-
+    private lateinit var _networkResult:NetworkResult
 
     override suspend fun searchBookDetailsInRemoteRepository(searchTitle:String,author:String,page:Int){
         Timber.d("find enhance book details called")
 
-        _response.value = null
+        _networkResult = Loading
         withContext(Dispatchers.IO){
             Timber.d("Starting network call")
             LibriVoxApi.retrofitService.searchBookInRemoteRepository(
@@ -57,7 +48,7 @@ class EnhanceDetailsRepositoryImpl : EnhanceDetailsRepository {
             ).enqueue(object : Callback<String> {
                 override fun onFailure(call: Call<String>, t: Throwable) {
                     Timber.d("Failure occur")
-                    _response.value = Event(Failure(Error(t.message)))
+                    _networkResult = Failure(Error(t.message))
                 }
 
                 override fun onResponse(call: Call<String>, response: Response<String>) {
@@ -65,22 +56,29 @@ class EnhanceDetailsRepositoryImpl : EnhanceDetailsRepository {
                     val result = gson.fromJson(response.body(), NetworkResponse::class.java)
                     Timber.d("Response got:${response.body()}")
                     result?.results?.let {
-                        _response.value = Event(Success(true))
-                        _bookSearchResult.value = result.results
-                        _listOfItem.value = BookBestMatchFromNetworkResult.getList(it)
-                        bestMatch = BookBestMatchFromNetworkResult.rank
+                        _networkResult = Success(true)
+                        _bookSearchResult = result.results
+                        _listOfItem = BookBestMatchFromNetworkResult.getList(it)
+                        _bestMatch = BookBestMatchFromNetworkResult.rank
                     }
                 }
             })
         }
     }
 
+    override fun getSearchBooksList(): List<WebDocument> = _listOfItem
+
+    override fun getBookListWithRanks(): List<Pair<Int, WebDocument>> = _bestMatch
+    override fun networkStatus(): NetworkResult = _networkResult
+
     override suspend fun fetchBookDetails(bookUrl:String){
         withContext(Dispatchers.IO){
             val details = BookDetailsParsingFromNetworkResponse.loadDetails(bookUrl)
             withContext(Dispatchers.Main){
-                _bookDetails.value = details
+                _bookDetails = details
             }
         }
     }
+
+    override fun getBookDetails(): BookDetails = _bookDetails
 }
