f849a33c78d2606dd4f3df852ce144950233ad4f
Pravinyo <pravinyo12@gmail.com>
2020-02-11 22:03:59 +0530

    unused methods removed and class renamed to ISearchBookDetailsRepository.kt

--- feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/domain/ISearchBookDetailsRepository.kt
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/domain/ISearchBookDetailsRepository.kt
@@ -1,20 +1,10 @@
 package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain
 
 import androidx.lifecycle.LiveData
-import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.BookDetails
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.WebDocument
-import com.allsoftdroid.common.base.network.NetworkResult
 
-interface EnhanceDetailsRepository {
+interface ISearchBookDetailsRepository : INetworkBaseRepository{
     suspend fun searchBookDetailsInRemoteRepository(searchTitle:String,author:String,page:Int)
     fun getSearchBooksList(): LiveData<List<WebDocument>>
     fun getBookListWithRanks(bookTitle:String,bookAuthor:String): LiveData<List<Pair<Int, WebDocument>>>
-
-    fun networkStatus() : NetworkResult
-
-    suspend fun fetchBookDetails(bookUrl:String)
-    fun getBookDetails():LiveData<BookDetails>
-
-    fun registerNetworkResponse(listener: NetworkResponseListener)
-    fun unRegisterNetworkResponse()
 }
