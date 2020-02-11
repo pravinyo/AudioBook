c9f6d323e28bd307af53868304de2090e6c7e0a9
Pravinyo <pravinyo12@gmail.com>
2020-02-11 22:01:37 +0530

    fetch book detail repository added

---
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/repository/FetchAdditionalBookDetailsRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.BookDetails
+import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain.IFetchAdditionBookDetailsRepository
+import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain.NetworkResponseListener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class FetchAdditionalBookDetailsRepositoryImpl : IFetchAdditionBookDetailsRepository {
+
+
+    private var _bookDetails = MutableLiveData<BookDetails>()
+
+    private var listener: NetworkResponseListener? = null
+
+    override fun registerNetworkResponse(listener: NetworkResponseListener){
+        this.listener = listener
+    }
+
+    override fun unRegisterNetworkResponse() {
+        this.listener = null
+    }
+
+    override suspend fun fetchBookDetails(bookUrl:String){
+        withContext(Dispatchers.IO){
+            val details = BookDetailsParsingFromNetworkResponse.loadDetails(bookUrl)
+            withContext(Dispatchers.Main){
+                _bookDetails.value = details
+            }
+        }
+    }
+
+    override fun getBookDetails(): LiveData<BookDetails> = _bookDetails
+}
