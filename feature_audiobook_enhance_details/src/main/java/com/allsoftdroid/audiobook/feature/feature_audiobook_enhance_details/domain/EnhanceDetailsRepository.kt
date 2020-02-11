9b91b9ee26aace72f26544c69205277cc4bdd27b
Pravinyo <pravinyo12@gmail.com>
2020-02-11 19:27:40 +0530

    network listener added and return type changed to liveData

--- feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/domain/EnhanceDetailsRepository.kt
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/domain/EnhanceDetailsRepository.kt
@@ -1,16 +1,20 @@
 package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain
 
+import androidx.lifecycle.LiveData
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.BookDetails
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.WebDocument
 import com.allsoftdroid.common.base.network.NetworkResult
 
 interface EnhanceDetailsRepository {
     suspend fun searchBookDetailsInRemoteRepository(searchTitle:String,author:String,page:Int)
-    fun getSearchBooksList(): List<WebDocument>
-    fun getBookListWithRanks(): List<Pair<Int, WebDocument>>
+    fun getSearchBooksList(): LiveData<List<WebDocument>>
+    fun getBookListWithRanks(bookTitle:String,bookAuthor:String): LiveData<List<Pair<Int, WebDocument>>>
 
     fun networkStatus() : NetworkResult
 
     suspend fun fetchBookDetails(bookUrl:String)
-    fun getBookDetails():BookDetails
+    fun getBookDetails():LiveData<BookDetails>
+
+    fun registerNetworkResponse(listener: NetworkResponseListener)
+    fun unRegisterNetworkResponse()
 }
