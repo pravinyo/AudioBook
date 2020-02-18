0ad8c1e8fa7e3cdfb342c879606839d29fce6f92
Pravinyo <pravinyo12@gmail.com>
2020-02-18 21:45:37 +0530

    functions are wrapped under class file

---
+++ feature_audiobook_enhance_details/src/main/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/data/network/LibriVoxApi.kt
@@ -0,0 +1,44 @@
+package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.network
+
+import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.network.request.ILibriVoxApiService
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+class LibriVoxApi{
+
+    companion object{
+        /**
+         * API is accessible via this object
+         */
+        val retrofitService:ILibriVoxApiService by lazy {
+            retrofit.create(ILibriVoxApiService::class.java)
+        }
+
+        private fun getHttpClientListener(): OkHttpClient {
+            val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+
+            return OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .readTimeout(30,TimeUnit.SECONDS)
+                .connectTimeout(30,TimeUnit.SECONDS)
+                .build()
+        }
+
+        /**
+         * create retrofit instance
+         */
+        private val retrofit = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .baseUrl(Utils.Books.getBaseURL())
+            .client(getHttpClientListener())
+            .build()
+
+    }
+}
+
