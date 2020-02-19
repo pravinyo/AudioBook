bf829d6227d011a244378655b5eaa5b316470ba0
Pravinyo <pravinyo12@gmail.com>
2020-02-19 21:51:27 +0530

    dependency interface renamed

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/usecase/GetTrackListUsecase.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/domain/usecase/GetTrackListUsecase.kt
@@ -2,24 +2,24 @@ package com.allsoftdroid.feature.book_details.domain.usecase
 
 import com.allsoftdroid.common.base.extension.Event
 import com.allsoftdroid.common.base.usecase.BaseUseCase
-import com.allsoftdroid.feature.book_details.data.repository.TrackFormat
-import com.allsoftdroid.feature.book_details.domain.repository.AudioBookMetadataRepository
+import com.allsoftdroid.feature.book_details.data.model.TrackFormat
+import com.allsoftdroid.feature.book_details.domain.repository.ITrackListRepository
 import timber.log.Timber
 
 class GetTrackListUsecase(
-    private val metadataRepository: AudioBookMetadataRepository):
+    private val listRepository: ITrackListRepository):
     BaseUseCase<GetTrackListUsecase.RequestValues,GetTrackListUsecase.ResponseValues>(){
 
     override suspend fun executeUseCase(requestValues: RequestValues?) {
         requestValues?.let {
-            metadataRepository.loadTrackListData(requestValues.trackFormat)
+            listRepository.loadTrackListData(requestValues.trackFormat)
             Timber.d("fetching started")
 
             useCaseCallback?.onSuccess(ResponseValues(Event(Unit)))
         }?:useCaseCallback?.onError(Error("Request value should not be null"))
     }
 
-    fun getTrackListData() = metadataRepository.getTrackList()
+    fun getTrackListData() = listRepository.getTrackList()
 
     class RequestValues(val trackFormat: TrackFormat) : BaseUseCase.RequestValues
     class ResponseValues (val event : Event<Any>) : BaseUseCase.ResponseValues
