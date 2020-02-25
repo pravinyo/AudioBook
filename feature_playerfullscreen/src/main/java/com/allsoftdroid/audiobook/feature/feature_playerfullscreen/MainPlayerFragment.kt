01e0e38650294241caa201be586616ecc9b122ba
Pravinyo <pravinyo12@gmail.com>
2020-02-25 17:22:58 +0530

    viewmodel dependency injected

--- feature_playerfullscreen/src/main/java/com/allsoftdroid/audiobook/feature/feature_playerfullscreen/MainPlayerFragment.kt
+++ feature_playerfullscreen/src/main/java/com/allsoftdroid/audiobook/feature/feature_playerfullscreen/MainPlayerFragment.kt
@@ -8,11 +8,14 @@ import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
+import com.allsoftdroid.audiobook.feature.feature_playerfullscreen.di.FeatureMainPlayerModule
 import com.allsoftdroid.common.base.fragment.BaseContainerFragment
+import org.koin.android.ext.android.inject
 
 class MainPlayerFragment : BaseContainerFragment(){
 
     private lateinit var callback: OnBackPressedCallback
+    private val booksViewModel: MainPlayerViewModel by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,10 +23,13 @@ class MainPlayerFragment : BaseContainerFragment(){
         savedInstanceState: Bundle?
     ): View? {
 
+        FeatureMainPlayerModule.injectFeature()
+
         val view = inflater.inflate(R.layout.layout_main_fragment,container,false)
         view.findViewById<ImageView>(R.id.toolbar_back_button).setOnClickListener {
             handleBackPressEvent()
         }
+
         return view
     }
 
