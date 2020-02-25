baef9b5612d3a7633a23cb577ae462b637a56525
Pravinyo <pravinyo12@gmail.com>
2020-02-25 17:23:20 +0530

    di inject module handler

---
+++ feature_playerfullscreen/src/main/java/com/allsoftdroid/audiobook/feature/feature_playerfullscreen/di/FeatureMainPlayerModule.kt
@@ -0,0 +1,58 @@
+package com.allsoftdroid.audiobook.feature.feature_playerfullscreen.di
+
+import androidx.annotation.VisibleForTesting
+import com.allsoftdroid.audiobook.feature.feature_playerfullscreen.MainPlayerViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.unloadKoinModules
+import org.koin.core.module.Module
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import kotlin.coroutines.CoroutineContext
+
+object FeatureMainPlayerModule {
+
+    fun injectFeature() = loadFeature
+
+    fun unLoadModules(){
+        unloadKoinModules(
+            listOf(
+                mainPlayerViewModelModule,
+                jobModule
+            )
+        )
+    }
+
+    private val loadFeature by lazy {
+        loadKoinModules(listOf(
+            mainPlayerViewModelModule,
+            jobModule
+        ))
+    }
+
+    var mainPlayerViewModelModule : Module = module {
+        viewModel{
+            MainPlayerViewModel()
+        }
+    }
+        @VisibleForTesting set
+
+    var jobModule : Module = module {
+
+        single(named(name = SUPER_VISOR_JOB)) {
+            SupervisorJob()
+        }
+
+        factory(named(name = VIEW_MODEL_SCOPE)) {
+            CoroutineScope(get(named(name = SUPER_VISOR_JOB)) as CoroutineContext + Dispatchers.Main)
+        }
+    }
+        @VisibleForTesting set
+
+
+    const val SUPER_VISOR_JOB = "SuperVisorJob"
+    const val VIEW_MODEL_SCOPE = "ViewModelScope"
+}
