8978c30afa3fd27246fce1b4061afc203433dfa2
Pravinyo <pravinyo12@gmail.com>
2020-02-28 23:04:03 +0530

    methods added for button controls and event sending to bus

--- feature_playerfullscreen/src/main/java/com/allsoftdroid/audiobook/feature/feature_playerfullscreen/MainPlayerViewModel.kt
+++ feature_playerfullscreen/src/main/java/com/allsoftdroid/audiobook/feature/feature_playerfullscreen/MainPlayerViewModel.kt
@@ -3,16 +3,21 @@ package com.allsoftdroid.audiobook.feature.feature_playerfullscreen
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.allsoftdroid.audiobook.feature.feature_playerfullscreen.data.PlayerControlState
 import com.allsoftdroid.audiobook.feature.feature_playerfullscreen.data.PlayingTrackDetails
 import com.allsoftdroid.audiobook.feature.feature_playerfullscreen.di.FeatureMainPlayerModule.SUPER_VISOR_JOB
 import com.allsoftdroid.audiobook.feature.feature_playerfullscreen.di.FeatureMainPlayerModule.VIEW_MODEL_SCOPE
+import com.allsoftdroid.common.base.extension.Event
+import com.allsoftdroid.common.base.extension.PlayingState
+import com.allsoftdroid.common.base.store.audioPlayer.*
 import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.CoroutineScope
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import org.koin.core.qualifier.named
+import timber.log.Timber
 
-class MainPlayerViewModel : ViewModel(), KoinComponent {
+class MainPlayerViewModel(private val eventStore : AudioPlayerEventStore) : ViewModel(), KoinComponent {
 
     /**
      * cancelling this job cancels all the job started by this viewmodel
@@ -24,17 +29,102 @@ class MainPlayerViewModel : ViewModel(), KoinComponent {
      */
     private val viewModelScope : CoroutineScope by inject(named(name = VIEW_MODEL_SCOPE))
 
+    private var _playerControlState = MutableLiveData<Event<PlayerControlState>>()
+    val previousControlState : LiveData<Event<PlayerControlState>> = _playerControlState
+
+    private var _shouldItPlay:Boolean = true
+    var shouldItPlay  = MutableLiveData<Boolean>()
+
     private var _playingTrackDetails = MutableLiveData<PlayingTrackDetails>()
     val playingTrackDetails:LiveData<PlayingTrackDetails> = _playingTrackDetails
 
-    fun setBookIdentifier(identifier:String){
-        _playingTrackDetails.value = PlayingTrackDetails(
-            bookIdentifier = identifier,
-            bookTitle = "Book Title goes here",
-            name = "Track Name goes here",
-            chapterIndex = 1,
-            totalChapter = 30
+    private var currentPlayingIndex = 0
+
+    fun playPrevious(){
+        _playerControlState.value = Event(PlayerControlState(playPrevious = true))
+
+        Timber.d("Sending new Previous event")
+        eventStore.publish(
+            Event(
+                Previous(
+                    PlayingState(
+                        playingItemIndex = currentPlayingIndex - 1,
+                        action_need = true
+                    )
+                )
+            )
         )
+    }
+
+    fun playNext(){
+        _playerControlState.value = Event(PlayerControlState(playNext = true))
+
+        Timber.d("Sending new next event")
+        eventStore.publish(
+            Event(
+                Next(
+                    PlayingState(
+                        playingItemIndex = currentPlayingIndex + 1,
+                        action_need = true
+                    )
+                )
+            )
+        )
+    }
+
+    fun playPause(){
+        _playerControlState.value = Event(PlayerControlState(shouldItPlay = _shouldItPlay))
+
+        setShouldPlay(!_shouldItPlay)
+    }
+
+    fun setBookDetails(bookId:String, bookName:String, trackName:String, currentPlayingTrack: Int, totalChapter:Int){
+
+        _playingTrackDetails.value = PlayingTrackDetails(
+            bookIdentifier = bookId,
+            bookTitle = bookName,
+            name = trackName,
+            chapterIndex = currentPlayingTrack,
+            totalChapter = totalChapter
+        )
+
+        currentPlayingIndex = currentPlayingTrack
+    }
+
+    fun setShouldPlay(play:Boolean){
+        _shouldItPlay = play
+        shouldItPlay.value = _shouldItPlay
+        shouldPlayEvent()
+    }
+
+    private fun shouldPlayEvent(){
+        Timber.d("should play event is $_shouldItPlay")
+
+        if(_shouldItPlay){
+            Timber.d("Sending new play event")
+            eventStore.publish(
+                Event(
+                    Play(
+                        PlayingState(
+                            playingItemIndex = currentPlayingIndex,
+                            action_need = true
+                        )
+                    )
+                )
+            )
+        }else{
+            Timber.d("Sending new pause event")
+            eventStore.publish(
+                Event(
+                    Pause(
+                        PlayingState(
+                            playingItemIndex = currentPlayingIndex,
+                            action_need = true
+                        )
+                    )
+                )
+            )
+        }
     }
 
     override fun onCleared() {
