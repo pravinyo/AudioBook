b2a7129f55cd30284bcbbf501417fbff6e8a77c4
Pravinyo <pravinyo12@gmail.com>
2020-02-28 23:02:48 +0530

    data class PlayerControlState.kt added to handle states of player control button

---
+++ feature_playerfullscreen/src/main/java/com/allsoftdroid/audiobook/feature/feature_playerfullscreen/data/PlayerControlState.kt
@@ -0,0 +1,7 @@
+package com.allsoftdroid.audiobook.feature.feature_playerfullscreen.data
+
+data class PlayerControlState(
+    var playNext:Boolean = false,
+    var playPrevious:Boolean= false,
+    var shouldItPlay:Boolean= false
+)
