9789c07cfa9ca2da4b132f2712a078fdc702c325
Pravinyo <pravinyo12@gmail.com>
2020-02-28 23:05:50 +0530

    Audio bus event observer added and redundant dependencies removed

--- feature_playerfullscreen/src/main/java/com/allsoftdroid/audiobook/feature/feature_playerfullscreen/MainPlayerFragment.kt
+++ feature_playerfullscreen/src/main/java/com/allsoftdroid/audiobook/feature/feature_playerfullscreen/MainPlayerFragment.kt
@@ -4,20 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
 import com.allsoftdroid.audiobook.feature.feature_playerfullscreen.databinding.LayoutMainFragmentBinding
 import com.allsoftdroid.audiobook.feature.feature_playerfullscreen.di.FeatureMainPlayerModule
 import com.allsoftdroid.common.base.fragment.BaseContainerFragment
+import com.allsoftdroid.common.base.store.audioPlayer.*
+import io.reactivex.disposables.Disposable
 import org.koin.android.ext.android.inject
+import timber.log.Timber
 
 class MainPlayerFragment : BaseContainerFragment(){
 
     private lateinit var callback: OnBackPressedCallback
     private val mainPlayerViewModel: MainPlayerViewModel by inject()
-    private val bookId = "tom_sawyer_librivox"
+    private val eventStore : AudioPlayerEventStore by inject()
+
+    private lateinit var dispose : Disposable
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,13 +32,40 @@ class MainPlayerFragment : BaseContainerFragment(){
         FeatureMainPlayerModule.injectFeature()
         val binding:LayoutMainFragmentBinding = inflateLayout(inflater,R.layout.layout_main_fragment,container)
 
+        binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = mainPlayerViewModel
 
         binding.toolbarBackButton.setOnClickListener {
             handleBackPressEvent()
         }
 
-        mainPlayerViewModel.setBookIdentifier(bookId)
+
+        dispose = eventStore.observe()
+            .subscribe {
+                Timber.d("Peeking event default")
+                it.peekContent().let {event ->
+                    when(event){
+                        is TrackDetails -> {
+                            Timber.d("Received event for update track details event")
+                            mainPlayerViewModel.setBookDetails(
+                                bookId= event.bookId,
+                                bookName = "Unknown",
+                                trackName = event.trackTitle,
+                                currentPlayingTrack = event.position,
+                                totalChapter = 0)
+                        }
+
+                        is Play, is Next, is Previous -> {
+                            mainPlayerViewModel.setShouldPlay(play = true)
+                        }
+
+                        is Pause -> {
+                            mainPlayerViewModel.setShouldPlay(play = false)
+                        }
+                    }
+                }
+            }
+
 
         return binding.root
     }
@@ -51,5 +82,10 @@ class MainPlayerFragment : BaseContainerFragment(){
 
     private fun handleBackPressEvent(){
         this.findNavController().navigateUp()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        dispose.dispose()
     }
 }
