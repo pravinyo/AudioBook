248b6d258d1a11950ba191aa81ec55b570c294c1
Pravinyo <pravinyo12@gmail.com>
2020-04-21 19:10:40 +0530

    DownloadStatus.java class added for status code

---
+++ feature_downloader/src/main/java/com/allsoftdroid/audiobook/feature_downloader/utils/DownloadStatus.java
@@ -0,0 +1,8 @@
+package com.allsoftdroid.audiobook.feature_downloader.utils;
+
+public interface DownloadStatus {
+    long DOWNLOADER_PROTOCOL_NOT_SUPPORTED=-444;
+    long DOWNLOADER_NOT_DOWNLOADING=-145;
+    long DOWNLOADER_PENDING_ID = 0;
+    long DOWNLOADER_RE_DOWNLOAD = -99;
+}
