0dfb7705076d3b9c789d9191ccaeb16722d3e83d
Pravinyo <pravinyo12@gmail.com>
2020-04-27 13:51:51 +0530

    Unit test for BestBookDetailsParser

---
+++ feature_audiobook_enhance_details/src/test/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/utils/BestBookDetailsParserTest.kt
@@ -0,0 +1,20 @@
+package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.utils
+
+import org.junit.Assert.*
+
+class BestBookDetailsParserTest {
+
+    @org.junit.Test
+    fun getList() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    @org.junit.Test
+    fun getListWithRanks() {
+        //Arrange
+        //Act
+        //Assert
+    }
+}
