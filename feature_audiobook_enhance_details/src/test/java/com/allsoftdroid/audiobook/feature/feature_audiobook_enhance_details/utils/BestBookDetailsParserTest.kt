264c19643ef7d677fa9121ad692b0696f88aa67b
Pravinyo <pravinyo12@gmail.com>
2020-04-27 14:36:37 +0530

    getListWebDocument implemented

--- feature_audiobook_enhance_details/src/test/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/utils/BestBookDetailsParserTest.kt
+++ feature_audiobook_enhance_details/src/test/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/utils/BestBookDetailsParserTest.kt
@@ -1,20 +1,33 @@
 package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.utils
 
-import org.junit.Assert.*
+import org.junit.Assert
+import org.junit.Test
+import java.io.File
 
 class BestBookDetailsParserTest {
 
-    @org.junit.Test
-    fun getList() {
+    private val systemUnderTest = BestBookDetailsParser()
+
+    @Test
+    fun getListOfWebDocuments_nonEmpty_returnsList() {
+        //Arrange
+        val htmlResponse:String = getHtmlResponse()
+        //Act
+        val result = systemUnderTest.getList(htmlResponse)
+        //Assert
+        Assert.assertNotEquals(0,result.size)
+    }
+
+    @Test
+    fun getListWithRanks() {
         //Arrange
         //Act
         //Assert
     }
 
-    @org.junit.Test
-    fun getListWithRanks() {
-        //Arrange
-        //Act
-        //Assert
+
+    private fun getHtmlResponse(): String {
+        val response = ClassLoader.getSystemResource("response.html")
+        return response.readText(Charsets.UTF_8)
     }
 }
