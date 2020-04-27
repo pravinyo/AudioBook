69f236d26bd98b8a1b9a84cd9c9e2a6786fef9a2
Pravinyo <pravinyo12@gmail.com>
2020-04-27 15:02:59 +0530

    added test for BestBookDetailsParser class

--- feature_audiobook_enhance_details/src/test/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/utils/BestBookDetailsParserTest.kt
+++ feature_audiobook_enhance_details/src/test/java/com/allsoftdroid/audiobook/feature/feature_audiobook_enhance_details/utils/BestBookDetailsParserTest.kt
@@ -2,16 +2,17 @@ package com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.uti
 
 import org.junit.Assert
 import org.junit.Test
-import java.io.File
 
 class BestBookDetailsParserTest {
 
     private val systemUnderTest = BestBookDetailsParser()
+    private val responsePath = "response.html"
+    private val noResponse = "noResult.html"
 
     @Test
     fun getListOfWebDocuments_nonEmpty_returnsList() {
         //Arrange
-        val htmlResponse:String = getHtmlResponse()
+        val htmlResponse:String = getResponse(isFound = true)
         //Act
         val result = systemUnderTest.getList(htmlResponse)
         //Assert
@@ -19,15 +20,43 @@ class BestBookDetailsParserTest {
     }
 
     @Test
-    fun getListWithRanks() {
+    fun getListOfWebDocuments_Empty_returnsList() {
         //Arrange
+        val htmlResponse:String = getResponse(isFound = false)
         //Act
+        val result = systemUnderTest.getList(htmlResponse)
         //Assert
+        Assert.assertEquals(0,result.size)
     }
 
+    @Test
+    fun getListWithRanks_returnHighRankItem() {
+        //Arrange
+        val htmlResponse:String = getResponse(isFound = true)
+        //Act
+        val result = systemUnderTest.getList(htmlResponse)
+        val ranks = systemUnderTest.getListWithRanks(result,"poem","Frank Oliver CALL (1878 - 1956)")
+        //Assert
+        Assert.assertNotEquals(0,ranks.size)
+    }
 
-    private fun getHtmlResponse(): String {
-        val response = ClassLoader.getSystemResource("response.html")
+    @Test
+    fun getListWithRanks_returnEmpty() {
+        //Arrange
+        val htmlResponse:String = getResponse(isFound = false)
+        //Act
+        val result = systemUnderTest.getList(htmlResponse)
+        val ranks = systemUnderTest.getListWithRanks(result,"poem","Frank Oliver CALL (1878 - 1956)")
+        //Assert
+        Assert.assertEquals(0,ranks.size)
+    }
+
+    private fun getResponse(isFound:Boolean):String{
+        return if (isFound) getHtmlResponse(responsePath) else getHtmlResponse(noResponse)
+    }
+
+    private fun getHtmlResponse(path:String): String {
+        val response = ClassLoader.getSystemResource(path)
         return response.readText(Charsets.UTF_8)
     }
 }
