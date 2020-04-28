795dde34cc3dc307aacb1a92fc5104a651eb8a88
Pravinyo <pravinyo12@gmail.com>
2020-04-28 20:06:24 +0530

    path changed

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/utils/TextFormatter.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/utils/TextFormatter.kt
@@ -4,56 +4,54 @@ import android.graphics.Color
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.BookDetails
 import com.allsoftdroid.feature.book_details.domain.model.AudioBookMetadataDomainModel
 
-class TextFormatter {
-    companion object{
-        fun formattedBookDetails(bookDetails: AudioBookMetadataDomainModel):CharSequence{
-            val labelAuthor ="Author:\t"
-            val labelRuntime ="\n\nRuntime:\t"
-            val labelDescription = "\n\nDescription:\t"
+object TextFormatter {
+    fun formattedBookDetails(bookDetails: AudioBookMetadataDomainModel):CharSequence{
+        val labelAuthor ="Author:\t"
+        val labelRuntime ="\n\nRuntime:\t"
+        val labelDescription = "\n\nDescription:\t"
 
-            return Truss()
-                .label(labelAuthor, Color.BLUE)
-                .append(bookDetails.creator)
+        return Truss()
+            .label(labelAuthor, Color.BLUE)
+            .append(bookDetails.creator)
 
-                .label(labelRuntime, Color.BLUE)
-                .append(bookDetails.runtime)
+            .label(labelRuntime, Color.BLUE)
+            .append(bookDetails.runtime)
 
-                .label(labelDescription, Color.BLUE)
-                .append(convertHtmlToText(bookDetails.description))
-                .build()
-        }
+            .label(labelDescription, Color.BLUE)
+            .append(convertHtmlToText(bookDetails.description))
+            .build()
+    }
 
-        fun formattedBookDetails(bookDetails: BookDetails):CharSequence{
-            val labelAuthor ="Author:\t"
-            val labelExtra ="\n\nExtra:\t"
-            val labelLanguage="\n\nLanguage:\t"
-            val labelRuntime ="\n\nRuntime:\t"
-            val labelGenres = "\n\nGenre(s):\t"
-            val labelReadText = "\n\nRead Text:\t"
-            val labelDescription = "\n\nDescription:\t"
+    fun formattedBookDetails(bookDetails: BookDetails):CharSequence{
+        val labelAuthor ="Author:\t"
+        val labelExtra ="\n\nExtra:\t"
+        val labelLanguage="\n\nLanguage:\t"
+        val labelRuntime ="\n\nRuntime:\t"
+        val labelGenres = "\n\nGenre(s):\t"
+        val labelReadText = "\n\nRead Text:\t"
+        val labelDescription = "\n\nDescription:\t"
 
-            return Truss()
-                .label(labelAuthor, Color.BLUE)
-                .append(bookDetails.webDocument?.author?:"NA")
+        return Truss()
+            .label(labelAuthor, Color.BLUE)
+            .append(bookDetails.webDocument?.author?:"NA")
 
-                .label(labelExtra, Color.BLUE)
-                .append(bookDetails.webDocument?.list.toString())
+            .label(labelExtra, Color.BLUE)
+            .append(bookDetails.webDocument?.list.toString())
 
-                .label(labelLanguage, Color.BLUE)
-                .append(bookDetails.language)
+            .label(labelLanguage, Color.BLUE)
+            .append(bookDetails.language)
 
-                .label(labelRuntime, Color.BLUE)
-                .append(bookDetails.runtime)
+            .label(labelRuntime, Color.BLUE)
+            .append(bookDetails.runtime)
 
-                .label(labelGenres, Color.BLUE)
-                .append(bookDetails.genres)
+            .label(labelGenres, Color.BLUE)
+            .append(bookDetails.genres)
 
-                .label(labelReadText, Color.BLUE)
-                .append(bookDetails.gutenbergUrl)
+            .label(labelReadText, Color.BLUE)
+            .append(bookDetails.gutenbergUrl)
 
-                .label(labelDescription, Color.BLUE)
-                .append(bookDetails.description)
-                .build()
-        }
+            .label(labelDescription, Color.BLUE)
+            .append(bookDetails.description)
+            .build()
     }
 }
