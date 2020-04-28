3e67b27ebd48d283c4b8d99cf1616b2fef0be270
Pravinyo <pravinyo12@gmail.com>
2020-04-28 20:07:44 +0530

    empty test file created for TextFormatter

---
+++ feature_book_details/src/test/java/com/allsoftdroid/feature/book_details/utils/TextFormatterTest.java
@@ -0,0 +1,21 @@
+package com.allsoftdroid.feature.book_details.utils;
+
+
+import org.junit.Test;
+
+public class TextFormatterTest {
+
+    @Test
+    public void formattedBookDetails() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    @Test
+    public void testFormattedBookDetails() {
+        //Arrange
+        //Act
+        //Assert
+    }
+}
