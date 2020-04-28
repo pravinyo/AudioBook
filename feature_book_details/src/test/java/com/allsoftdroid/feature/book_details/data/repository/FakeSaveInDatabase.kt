810aef992fa1ff23541ad80409120692db626c88
Pravinyo <pravinyo12@gmail.com>
2020-04-28 21:42:21 +0530

    fake data source created

---
+++ feature_book_details/src/test/java/com/allsoftdroid/feature/book_details/data/repository/FakeSaveInDatabase.kt
@@ -0,0 +1,29 @@
+package com.allsoftdroid.feature.book_details.data.repository
+
+import com.allsoftdroid.database.common.SaveInDatabase
+import com.allsoftdroid.database.metadataCacheDB.MetadataDao
+import com.allsoftdroid.feature.book_details.data.model.AudioBookMetadataDataModel
+import timber.log.Timber
+
+class FakeSaveInDatabase(private val dao : MetadataDao) : SaveInDatabase<MetadataDao, FakeSaveInDatabase> {
+    override var mDao: MetadataDao = dao
+
+    private lateinit var mBookList: List<AudioBookMetadataDataModel>
+
+    override fun addData(data: Any): FakeSaveInDatabase {
+        mBookList = (data as List<*>).filterIsInstance<AudioBookMetadataDataModel>()
+
+        return this
+    }
+
+    override suspend fun execute() {
+
+        //scan the list and build the new to be inserted in the database
+        for(element in mBookList){
+            val book: AudioBookMetadataDataModel = element
+
+            Timber.i(book.identifier)
+            Timber.i(book.title)
+        }
+    }
+}
