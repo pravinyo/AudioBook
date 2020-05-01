61c660ca90b71c36bac2bf09e5dd8165cb6d53f1
Pravinyo <pravinyo12@gmail.com>
2020-05-01 19:59:19 +0530

    starter code for Book detail fragment

---
+++ feature_book_details/src/androidTest/java/com/allsoftdroid/feature/book_details/presentation/AudioBookDetailsFragmentTest.kt
@@ -0,0 +1,61 @@
+package com.allsoftdroid.feature.book_details.presentation
+
+import android.app.Application
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.allsoftdroid.common.base.usecase.UseCaseHandler
+import com.allsoftdroid.feature.book_details.R
+import com.allsoftdroid.feature.book_details.data.repository.BookDetailsSharedPreferencesRepositoryImpl
+import com.allsoftdroid.feature.book_details.domain.repository.BookDetailsSharedPreferenceRepository
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class AudioBookDetailsFragmentTest{
+
+    @Before
+    fun setup(){
+
+        val application = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext as Application
+        val dataModuleTest : Module = module {
+            single<BookDetailsSharedPreferenceRepository> {
+                BookDetailsSharedPreferencesRepositoryImpl.create(application)
+            }
+        }
+
+        val usecaseHandlerModuleTest : Module = module {
+            single {
+                UseCaseHandler.getInstance()
+            }
+        }
+
+        startKoin {
+            modules(dataModuleTest,usecaseHandlerModuleTest)
+        }
+    }
+
+    @Test
+    fun audioBookDetails_DisplayedInUi() {
+
+        // WHEN - Details fragment launched to display task
+        val bundle = bundleOf()
+        launchFragmentInContainer<AudioBookDetailsFragment>(bundle, R.style.AppTheme)
+        Thread.sleep(2000)
+    }
+
+    @After
+    fun teardown(){
+        stopKoin()
+    }
+
+}
