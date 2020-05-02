10c52d194774d389f0b8b58fc13380da35cf6544
Pravinyo <pravinyo12@gmail.com>
2020-05-02 11:38:25 +0530

    method definition added

--- feature_book_details/src/androidTest/java/com/allsoftdroid/feature/book_details/presentation/utils/FakeBookDetailsRepository.kt
+++ feature_book_details/src/androidTest/java/com/allsoftdroid/feature/book_details/presentation/utils/FakeBookDetailsRepository.kt
@@ -5,15 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.data.model.BookDetails
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain.network.NetworkResponseListener
 import com.allsoftdroid.audiobook.feature.feature_audiobook_enhance_details.domain.repository.IFetchAdditionBookDetailsRepository
+import com.allsoftdroid.common.base.network.Success
 
 class FakeBookDetailsRepository: IFetchAdditionBookDetailsRepository {
 
     private val details = MutableLiveData<BookDetails>()
+    private var listener: NetworkResponseListener? = null
 
     override suspend fun fetchBookDetails(bookUrl: String) {
         details.value = BookDetails(
             null,"1",bookUrl,"url","","","", emptyList()
         )
+
+        listener?.onResponse(Success(result = "success"))
     }
 
     override fun getBookDetails(): LiveData<BookDetails> {
@@ -21,10 +25,10 @@ class FakeBookDetailsRepository: IFetchAdditionBookDetailsRepository {
     }
 
     override fun registerNetworkResponse(listener: NetworkResponseListener) {
-
+        this.listener = listener
     }
 
     override fun unRegisterNetworkResponse() {
-
+        listener = null
     }
 }
