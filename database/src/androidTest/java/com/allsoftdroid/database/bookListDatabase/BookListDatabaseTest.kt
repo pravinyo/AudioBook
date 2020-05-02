bda798c6d3cbac4ef4db58a2c57feca3e32fcc5b
Pravinyo <pravinyo12@gmail.com>
2020-05-03 00:56:54 +0530

    bug fixed

--- database/src/androidTest/java/com/allsoftdroid/database/bookListDatabase/BookListDatabaseTest.kt
+++ database/src/androidTest/java/com/allsoftdroid/database/bookListDatabase/BookListDatabaseTest.kt
@@ -1,29 +1,40 @@
-package com.allsoftdroid.database.BookListDatabase
+package com.allsoftdroid.database.bookListDatabase
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
 import com.allsoftdroid.database.bookListDB.AudioBookDao
 import com.allsoftdroid.database.bookListDB.DatabaseAudioBook
 import com.allsoftdroid.database.common.AudioBookDatabase
-import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
 
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
+@SmallTest
 class BooksDatabaseTest {
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
 
     private lateinit var bookListDao: AudioBookDao
     private lateinit var db: AudioBookDatabase
 
     @Before
     fun createDb() {
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        // Using an in-memory database because the information stored here disappears when the
-        // process is killed.
+        val context = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
+
         db = Room.inMemoryDatabaseBuilder(context, AudioBookDatabase::class.java)
             // Allowing main thread queries, just for testing.
             .allowMainThreadQueries()
@@ -40,7 +51,7 @@ class BooksDatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun test_BooksDatabase_Users_insertAndGetSuccess() {
+    fun test_BooksDatabase_Users_insertAndGetSuccess() = runBlockingTest{
         val user = DatabaseAudioBook(
             identifier = "identifier",
             title = "sample",
@@ -50,6 +61,6 @@ class BooksDatabaseTest {
 
         bookListDao.insert(user)
         val last = bookListDao.getLastBook()
-        assertEquals(last?.creator, "Pravin")
+        assertEquals(last.creator, "Pravin")
     }
 }
