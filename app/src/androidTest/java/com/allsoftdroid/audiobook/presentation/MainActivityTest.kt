5639df2a1e7904438d20fe86e9ff511f6938c7ac
Pravinyo <pravinyo12@gmail.com>
2020-05-04 00:13:58 +0530

    registered idling resource

--- app/src/androidTest/java/com/allsoftdroid/audiobook/presentation/MainActivityTest.kt
+++ app/src/androidTest/java/com/allsoftdroid/audiobook/presentation/MainActivityTest.kt
@@ -3,6 +3,7 @@ package com.allsoftdroid.audiobook.presentation
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -10,6 +11,11 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.allsoftdroid.audiobook.R
+import com.allsoftdroid.common.test.DataBindingIdlingResource
+import com.allsoftdroid.common.test.EspressoIdlingResource
+import com.allsoftdroid.common.test.monitorActivity
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,9 +23,24 @@ import org.junit.runner.RunWith
 @LargeTest
 class MainActivityTest{
 
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @Before
+    fun registerIdlingResource(){
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    @After
+    fun unRegisterIdlingResource(){
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
     @Test
     fun openActivity_DisplayInUi(){
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
 
         onView(withId(R.id.toolbar_title)).check(matches(withText("Latest Audio Books")))
 
