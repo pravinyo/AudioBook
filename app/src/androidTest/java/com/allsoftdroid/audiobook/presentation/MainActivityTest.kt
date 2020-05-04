50d8c11f06c9dae2e2488bd642d3b33be97b0b6b
Pravinyo <pravinyo12@gmail.com>
2020-05-04 12:33:49 +0530

    test added for search and book play

--- app/src/androidTest/java/com/allsoftdroid/audiobook/presentation/MainActivityTest.kt
+++ app/src/androidTest/java/com/allsoftdroid/audiobook/presentation/MainActivityTest.kt
@@ -1,13 +1,15 @@
 package com.allsoftdroid.audiobook.presentation
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.allsoftdroid.audiobook.R
@@ -38,7 +40,7 @@ class MainActivityTest{
     }
 
     @Test
-    fun openActivity_DisplayInUi(){
+    fun openDownloadsActivity_DisplayInUi(){
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
@@ -47,6 +49,99 @@ class MainActivityTest{
         onView(withId(R.id.toolbar_downloads)).perform(click())
 
         pressBack()
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun searchBook_DisplayInUi(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.toolbar_book_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.toolbar_book_search)).perform(click())
+
+        onView(withId(R.id.iv_search_cancel)).check(matches(isDisplayed()))
+        onView(withId(R.id.iv_search_cancel)).perform(click())
+
+        onView(withId(R.id.toolbar_title)).check(matches(withText("Latest Audio Books")))
+
+        onView(withId(R.id.toolbar_book_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.toolbar_book_search)).perform(click())
+
+        onView(withId(R.id.et_toolbar_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.et_toolbar_search)).perform(clearText(), typeText("poem"))
+
+        onView(withId(R.id.iv_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.iv_search)).perform(click())
+
+
+        Thread.sleep(7000)
+        onView(withId(R.id.recycler_view_books))
+            .check(matches(hasMinimumChildCount(1)))
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun openBookDetailsScreen_DisplayInUi(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        Thread.sleep(10000)
+        onView(withId(R.id.recycler_view_books))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+
+        onView(withId(R.id.tv_toolbar_title)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.btn_toolbar_back_arrow)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.textView_description)).check(matches(isDisplayed()))
+
+        pressBack()
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun scroll_down_openBookDetailsScreen_DisplayInUi(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        Thread.sleep(10000)
+        onView(withId(R.id.recycler_view_books))
+            .perform(scrollToPosition<RecyclerView.ViewHolder>(25))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(25,click()))
+
+
+        pressBack()
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun play_Book_from_BookDetailsScreen_DisplayInUi(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        Thread.sleep(10000)
+        onView(withId(R.id.recycler_view_books))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+
+        onView(withId(R.id.tv_toolbar_title)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.btn_toolbar_back_arrow)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.textView_description)).check(matches(isDisplayed()))
+
+        Thread.sleep(4000)
+        onView(withId(R.id.recyclerView))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+
+
+        Thread.sleep(2000)
+        onView(withId(R.id.miniPlayerContainer)).check(matches(isDisplayed()))
+
 
         activityScenario.close()
     }
