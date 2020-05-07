b0bb6bc38a341ccaf0f2c7a7ab801146585b31a0
Pravinyo <pravinyo12@gmail.com>
2020-05-07 21:02:35 +0530

    implementation added for downloads path

---
+++ feature_settings/src/main/java/com/allsoftdroid/audiobook/feature_settings/SettingsFragment.kt
@@ -0,0 +1,67 @@
+package com.allsoftdroid.audiobook.feature_settings
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.allsoftdroid.audiobook.feature_settings.utils.FileUtil
+import com.allsoftdroid.common.base.network.ArchiveUtils
+import com.allsoftdroid.common.base.utils.SettingsPreferenceUtils
+import timber.log.Timber
+
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    private val DOWNLOADS_FOLDER_CODE = 9999
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_preferences,rootKey)
+
+        setupDownloadPref()
+    }
+
+    private fun setupDownloadPref() {
+        activity?.let {
+            val downloadPref = findPreference<Preference>(SettingsPreferenceUtils.DOWNLOADS_KEY)
+
+            val path = ArchiveUtils.getDownloadsRootFolder(it.application)
+
+            downloadPref?.summary = path
+
+            downloadPref?.setOnPreferenceClickListener {
+                sendIntentForDirectoryPick()
+                return@setOnPreferenceClickListener true
+            }
+        }
+    }
+
+    private fun sendIntentForDirectoryPick() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        startActivityForResult(Intent.createChooser(intent,"Select Folder"),DOWNLOADS_FOLDER_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            DOWNLOADS_FOLDER_CODE -> {
+
+                data?.let {intent ->
+                    val treeUri = intent.data
+                    Timber.d("URL is $treeUri")
+
+                    activity?.let {activity ->
+                        val path = FileUtil.getFullPathFromTreeUri(treeUri,activity)
+
+                        path?.let {
+                            ArchiveUtils.setDownloadsRootFolder(activity.application,path)
+                        }
+                    }
+                }
+            }
+
+            else ->{}
+        }
+    }
+}
