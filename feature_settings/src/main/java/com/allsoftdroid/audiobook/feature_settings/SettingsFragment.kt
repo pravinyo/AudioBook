3019bc202728c36fe707abd24122b49bd1085df9
Pravinyo <pravinyo12@gmail.com>
2020-05-08 12:42:02 +0530

    allowed folder dialog added and fragment refresh added

--- feature_settings/src/main/java/com/allsoftdroid/audiobook/feature_settings/SettingsFragment.kt
+++ feature_settings/src/main/java/com/allsoftdroid/audiobook/feature_settings/SettingsFragment.kt
@@ -1,8 +1,14 @@
 package com.allsoftdroid.audiobook.feature_settings
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.allsoftdroid.audiobook.feature_settings.utils.FileUtil
@@ -14,6 +20,11 @@ import timber.log.Timber
 class SettingsFragment : PreferenceFragmentCompat() {
 
     private val DOWNLOADS_FOLDER_CODE = 9999
+    private val standardDirectory = listOf<String>(
+        Environment.DIRECTORY_DOWNLOADS,
+        Environment.DIRECTORY_DOCUMENTS,
+        Environment.DIRECTORY_MUSIC,
+        Environment.DIRECTORY_MOVIES)
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_preferences,rootKey)
@@ -24,16 +35,37 @@ class SettingsFragment : PreferenceFragmentCompat() {
     private fun setupDownloadPref() {
         activity?.let {
             val downloadPref = findPreference<Preference>(SettingsPreferenceUtils.DOWNLOADS_KEY)
-
             val path = ArchiveUtils.getDownloadsRootFolder(it.application)
 
             downloadPref?.summary = "/$path"
-
             downloadPref?.setOnPreferenceClickListener {
                 sendIntentForDirectoryPick()
                 return@setOnPreferenceClickListener true
             }
+
+            val downloadHelpPref = findPreference<Preference>(SettingsPreferenceUtils.DOWNLOADS_HELP_KEY)
+            downloadHelpPref?.setOnPreferenceClickListener {
+                showAllowedFolderDialog()
+                return@setOnPreferenceClickListener true
+            }
+
         }
+    }
+
+    private fun showAllowedFolderDialog() {
+        val builder = AlertDialog.Builder(this.requireContext())
+
+        builder.setTitle("Folders allowed")
+        builder.setMessage(TextUtils.join("\n",standardDirectory))
+
+        builder.setNegativeButton("Dismiss"){
+                dialog,_ ->
+            dialog.dismiss()
+        }
+
+        val dialog = builder.create()
+
+        dialog.show()
     }
 
     private fun sendIntentForDirectoryPick() {
@@ -60,7 +92,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
                             val folder = path.substring(root.length+1)
                             Timber.d("Path returned: $folder")
                             Timber.d("Path root older api: $root")
-                            ArchiveUtils.setDownloadsRootFolder(activity.application,folder)
+                            val directory = folder.split("/")[0]
+
+                            if(inStandardDirectory(directory)){
+                                ArchiveUtils.setDownloadsRootFolder(activity.application,folder)
+                                this.findNavController()
+                                    .navigate(R.id.SettingsFragment,null,NavOptions.Builder()
+                                        .setPopUpTo(R.id.SettingsFragment,true)
+                                        .build())
+                            }else{
+                                Toast.makeText(activity,"Please Press i for allowed directory",Toast.LENGTH_SHORT).show()
+                            }
                         }
                     }
                 }
@@ -68,5 +110,13 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
             else ->{}
         }
+    }
+
+    private fun inStandardDirectory(directory: String): Boolean {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if(directory == Environment.DIRECTORY_AUDIOBOOKS)  return true
+        }
+        return standardDirectory.contains(directory)
     }
 }
