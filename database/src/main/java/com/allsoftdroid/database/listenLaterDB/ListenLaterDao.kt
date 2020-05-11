8e2045b065be6524148fd3e5cee6087043e8343c
Pravinyo <pravinyo12@gmail.com>
2020-05-11 22:19:45 +0530

    ListenLaterDao added for data access of listen later db

---
+++ database/src/main/java/com/allsoftdroid/database/listenLaterDB/ListenLaterDao.kt
@@ -0,0 +1,30 @@
+package com.allsoftdroid.database.listenLaterDB
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.allsoftdroid.database.listenLaterDB.entity.DatabaseListenLaterEntity
+
+@Dao
+interface ListenLaterDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertForLater(listenLater : DatabaseListenLaterEntity)
+
+    @Query("DELETE FROM ListenLater_Table where book_id = :identifier")
+    fun removeById(identifier:String):Int
+
+    @Query("SELECT * FROM ListenLater_Table order by timestamp desc")
+    fun getBooksInLIFO():LiveData<List<DatabaseListenLaterEntity>>
+
+    @Query("SELECT * FROM ListenLater_Table order by timestamp asc")
+    fun getBooksInFIFO():LiveData<List<DatabaseListenLaterEntity>>
+
+    @Query("SELECT * FROM ListenLater_Table order by play_time")
+    fun getBooksInOrderOfLength():LiveData<List<DatabaseListenLaterEntity>>
+
+    @Query("SELECT count(*) FROM ListenLater_Table where book_id = :bookId")
+    fun getListenLaterStatusFor(bookId:String):Int
+}
