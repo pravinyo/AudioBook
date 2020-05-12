b9bd93d461da91b7d2fc9427edefd8f23fccb5f3
Pravinyo <pravinyo12@gmail.com>
2020-05-12 23:25:49 +0530

    data binding enabled in fragment

--- feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/ListenLaterFragment.kt
+++ feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/ListenLaterFragment.kt
@@ -1,11 +1,37 @@
 package com.allsoftdroid.audiobook.feature_listen_later_ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import com.allsoftdroid.audiobook.feature_listen_later_ui.databinding.FragmentListenLaterLayoutBinding
 import com.allsoftdroid.common.base.fragment.BaseUIFragment
+import org.koin.android.ext.android.inject
 
 class ListenLaterFragment : BaseUIFragment() {
 
+    val listenLaterViewModel:ListenLaterViewModel by inject()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val dataBinding:FragmentListenLaterLayoutBinding = inflateLayout(inflater,R.layout.fragment_listen_later_layout,container,false)
+        FeatureListenLaterModule.unLoadModules()
+
+        return dataBinding.root
+    }
+
     override fun handleBackPressEvent(callback: OnBackPressedCallback) {
-        TODO("Not yet implemented")
+        callback.isEnabled = false
+        requireActivity().onBackPressed()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        FeatureListenLaterModule.unLoadModules()
     }
 }
