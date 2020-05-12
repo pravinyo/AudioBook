475ab65115b50ebbd8dcb5c798c3bd0f839b546c
Pravinyo <pravinyo12@gmail.com>
2020-05-12 23:25:17 +0530

    listen later viewmodel added

---
+++ feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/ListenLaterViewModel.kt
@@ -0,0 +1,29 @@
+package com.allsoftdroid.audiobook.feature_listen_later_ui
+
+import androidx.lifecycle.ViewModel
+import com.allsoftdroid.audiobook.feature_listen_later_ui.FeatureListenLaterModule.SUPER_VISOR_JOB
+import com.allsoftdroid.audiobook.feature_listen_later_ui.FeatureListenLaterModule.VIEW_MODEL_SCOPE
+import kotlinx.coroutines.CompletableJob
+import kotlinx.coroutines.CoroutineScope
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import org.koin.core.qualifier.named
+
+class ListenLaterViewModel : ViewModel(), KoinComponent {
+    /**
+     * cancelling this job cancels all the job started by this viewmodel
+     */
+    private val viewModelJob: CompletableJob by inject(named(name = SUPER_VISOR_JOB))
+
+    /**
+     * main scope for all coroutine launched by viewmodel
+     */
+    private val viewModelScope : CoroutineScope by inject(named(name = VIEW_MODEL_SCOPE))
+
+
+    //cancel the job when viewmodel is not longer in use
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
