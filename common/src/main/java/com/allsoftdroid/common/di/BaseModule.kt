03a0e03499ee89d025378d4d49e13f822819c38e
Pravinyo <pravinyo12@gmail.com>
2020-05-12 15:37:27 +0530

    dependency for User action store added

--- common/src/main/java/com/allsoftdroid/common/di/BaseModule.kt
+++ common/src/main/java/com/allsoftdroid/common/di/BaseModule.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import com.allsoftdroid.common.base.network.ConnectionLiveData
 import com.allsoftdroid.common.base.store.audioPlayer.AudioPlayerEventBus
 import com.allsoftdroid.common.base.store.downloader.DownloaderEventBus
+import com.allsoftdroid.common.base.store.userAction.UserActionEventBus
+import com.allsoftdroid.common.base.store.userAction.UserActionEventStore
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.unloadKoinModules
 import org.koin.core.module.Module
@@ -16,6 +18,7 @@ object BaseModule {
         listOf(
             audioPlayerEventBusModule,
             downloaderEventBusModule,
+            userActionEventBusModule,
             connectivityModule
         )
     )
@@ -24,6 +27,7 @@ object BaseModule {
         loadKoinModules(listOf(
             audioPlayerEventBusModule,
             downloaderEventBusModule,
+            userActionEventBusModule,
             connectivityModule
         ))
     }
@@ -37,6 +41,12 @@ object BaseModule {
     private val downloaderEventBusModule : Module = module {
         single {
             DownloaderEventBus.getEventBusInstance()
+        }
+    }
+
+    private val userActionEventBusModule : Module = module {
+        single {
+            UserActionEventBus.getEventBusInstance()
         }
     }
 
