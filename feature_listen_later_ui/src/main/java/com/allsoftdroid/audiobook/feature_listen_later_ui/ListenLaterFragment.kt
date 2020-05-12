7b52b08843741e78e2b4d41f377693ebc914a871
Pravinyo <pravinyo12@gmail.com>
2020-05-12 21:06:04 +0530

    listen later module created

---
+++ feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/ListenLaterFragment.kt
@@ -0,0 +1,11 @@
+package com.allsoftdroid.audiobook.feature_listen_later_ui
+
+import androidx.activity.OnBackPressedCallback
+import com.allsoftdroid.common.base.fragment.BaseUIFragment
+
+class ListenLaterFragment : BaseUIFragment() {
+
+    override fun handleBackPressEvent(callback: OnBackPressedCallback) {
+        TODO("Not yet implemented")
+    }
+}
