27ac5fbc56d7549b4f530562aed669efecc12170
Pravinyo <pravinyo12@gmail.com>
2020-05-13 23:25:50 +0530

    ListenLaterViewModel added for bookmarked items

--- feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/presentation/ListenLaterViewModel.kt
+++ feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/presentation/ListenLaterViewModel.kt
@@ -1,15 +1,21 @@
 package com.allsoftdroid.audiobook.feature_listen_later_ui.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.allsoftdroid.audiobook.feature_listen_later_ui.di.FeatureListenLaterModule.SUPER_VISOR_JOB
 import com.allsoftdroid.audiobook.feature_listen_later_ui.di.FeatureListenLaterModule.VIEW_MODEL_SCOPE
+import com.allsoftdroid.audiobook.feature_listen_later_ui.domain.*
+import com.allsoftdroid.common.base.extension.Event
 import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import org.koin.core.qualifier.named
 
-class ListenLaterViewModel : ViewModel(), KoinComponent {
+class ListenLaterViewModel(private val repository:IListenLaterRepository) : ViewModel(), KoinComponent {
     /**
      * cancelling this job cancels all the job started by this viewmodel
      */
@@ -20,6 +26,28 @@ class ListenLaterViewModel : ViewModel(), KoinComponent {
      */
     private val viewModelScope : CoroutineScope by inject(named(name = VIEW_MODEL_SCOPE))
 
+
+    private var _requestStatus = MutableLiveData<Event<RequestStatus>>()
+    val requestStatus : LiveData<Event<RequestStatus>> = _requestStatus
+
+    init {
+        loadDefault()
+    }
+
+    private fun loadDefault() {
+        viewModelScope.launch {
+            _requestStatus.value = Event(Started)
+
+            val data =
+                withContext(viewModelScope.coroutineContext) { repository.getBooksInLIFO() }
+
+            if(!data.isNullOrEmpty()){
+                _requestStatus.value = Event(Success(data))
+            }else{
+                _requestStatus.value = Event(Empty)
+            }
+        }
+    }
 
     //cancel the job when viewmodel is not longer in use
     override fun onCleared() {
