d11d9d62899353509ec5bdd5bd3a3b9fa1234085
Pravinyo <pravinyo12@gmail.com>
2020-05-13 23:25:16 +0530

    fragment implementation added for Listen Later screen

--- feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/presentation/ListenLaterFragment.kt
+++ feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/presentation/ListenLaterFragment.kt
@@ -4,16 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.allsoftdroid.audiobook.feature_listen_later_ui.R
+import com.allsoftdroid.audiobook.feature_listen_later_ui.data.model.ListenLaterItemDomainModel
 import com.allsoftdroid.audiobook.feature_listen_later_ui.databinding.FragmentListenLaterLayoutBinding
 import com.allsoftdroid.audiobook.feature_listen_later_ui.di.FeatureListenLaterModule
+import com.allsoftdroid.audiobook.feature_listen_later_ui.domain.Empty
+import com.allsoftdroid.audiobook.feature_listen_later_ui.domain.Started
+import com.allsoftdroid.audiobook.feature_listen_later_ui.domain.Success
+import com.allsoftdroid.audiobook.feature_listen_later_ui.presentation.recyclerView.ItemClickedListener
+import com.allsoftdroid.audiobook.feature_listen_later_ui.presentation.recyclerView.ListenLaterAdapter
 import com.allsoftdroid.common.base.fragment.BaseUIFragment
 import org.koin.android.ext.android.inject
+import org.koin.core.KoinComponent
+import org.koin.core.qualifier.named
 
-class ListenLaterFragment : BaseUIFragment() {
+class ListenLaterFragment : BaseUIFragment(),KoinComponent {
 
-    val listenLaterViewModel: ListenLaterViewModel by inject()
+    private val listenLaterViewModel: ListenLaterViewModel by inject()
+    private lateinit var bindingRef:FragmentListenLaterLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FeatureListenLaterModule.injectFeature()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,18 +40,109 @@ class ListenLaterFragment : BaseUIFragment() {
 
         val dataBinding:FragmentListenLaterLayoutBinding = inflateLayout(inflater,
             R.layout.fragment_listen_later_layout,container,false)
-        FeatureListenLaterModule.unLoadModules()
+
+        dataBinding.lifecycleOwner = viewLifecycleOwner
+        dataBinding.viewModel = listenLaterViewModel
+
+        //val audio book adapter
+        val listenLaterAdapter = ListenLaterAdapter(ItemClickedListener {
+            Toast.makeText(this.requireActivity(),"Book Id:$it",Toast.LENGTH_SHORT).show()
+        })
+
+        //attach adapter to recycler view
+        dataBinding.recyclerViewBooks.adapter = listenLaterAdapter
+
+        //recycler view layout manager
+        dataBinding.recyclerViewBooks.apply {
+            layoutManager = LinearLayoutManager(context)
+        }
+
+        dataBinding.toolbarBackArrow.setOnClickListener {
+            onBackPressed()
+        }
+
+        listenLaterViewModel.requestStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { request ->
+                when(request){
+                    is Started -> {
+                        loading()
+                    }
+
+                    is Empty -> {
+                        noDataUI()
+                    }
+
+                    is Success -> {
+                        if (request.list.isEmpty()){
+                            noDataUI()
+                        }else
+                        {
+                            dataAvailable(request.list)
+                            listenLaterAdapter.submitList(request.list)
+                        }
+                    }
+                }
+            }
+        })
+
+        bindingRef = dataBinding
 
         return dataBinding.root
+    }
+
+    private fun noDataUI() {
+        removeLoading()
+    }
+
+    private fun dataAvailable(list: List<ListenLaterItemDomainModel>) {
+
+        bindingRef.bookStatsCount.apply {
+            this.text = "${list.size} Books"
+        }
+
+        bindingRef.loadingProgressbar.apply {
+            this.visibility = View.GONE
+        }
+
+        bindingRef.recyclerViewBooks.apply {
+            this.visibility = View.VISIBLE
+        }
+
+        bindingRef.bookStats.apply {
+            this.visibility = View.VISIBLE
+        }
+    }
+
+    private fun removeLoading(){
+        bindingRef.loadingProgressbar.apply {
+            this.visibility = View.GONE
+        }
+
+        bindingRef.noData.apply {
+            this.visibility = View.VISIBLE
+        }
+    }
+
+    private fun loading(){
+        bindingRef.loadingProgressbar.apply {
+            this.visibility = View.VISIBLE
+        }
+
+        bindingRef.noData.apply {
+            this.visibility = View.GONE
+        }
+
+        bindingRef.recyclerViewBooks.apply {
+            this.visibility = View.GONE
+        }
+
+        bindingRef.bookStats.apply {
+            this.visibility = View.GONE
+        }
     }
 
     override fun handleBackPressEvent(callback: OnBackPressedCallback) {
         callback.isEnabled = false
         requireActivity().onBackPressed()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        FeatureListenLaterModule.unLoadModules()
     }
 }
