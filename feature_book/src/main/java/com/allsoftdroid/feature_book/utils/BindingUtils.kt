76f6fdd16c08e7ffc1dfb13bfda6cbfd0810004d
Pravinyo <pravinyo12@gmail.com>
2020-05-16 12:16:31 +0530

    signature added to glide request

--- feature_book/src/main/java/com/allsoftdroid/feature_book/utils/BindingUtils.kt
+++ feature_book/src/main/java/com/allsoftdroid/feature_book/utils/BindingUtils.kt
@@ -1,21 +1,20 @@
 package com.allsoftdroid.feature_book.utils
 
-import android.content.Context
-import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.allsoftdroid.common.base.extension.CreateImageOverlay
-import com.allsoftdroid.common.base.network.ArchiveUtils.Companion.getThumbnail
+import com.allsoftdroid.common.base.utils.BindingUtils.convertDateToTime
+import com.allsoftdroid.common.base.utils.BindingUtils.getNormalizedText
+import com.allsoftdroid.common.base.utils.BindingUtils.getSignatureForImageLoading
+import com.allsoftdroid.common.base.utils.BindingUtils.getThumbnail
 import com.allsoftdroid.feature_book.R
 import com.allsoftdroid.feature_book.domain.model.AudioBookDomainModel
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
-import timber.log.Timber
-import java.text.SimpleDateFormat
-import java.util.*
+import com.bumptech.glide.signature.ObjectKey
 
 /**
 Handle visibility of progress bar
@@ -50,6 +49,7 @@ If content is not there show broken image
 fun setImageUrl(imageView: ImageView, item: AudioBookDomainModel?) {
 
     item?.let {
+
         val url = getThumbnail(item.mId)
 
         Glide
@@ -57,10 +57,10 @@ fun setImageUrl(imageView: ImageView, item: AudioBookDomainModel?) {
             .asBitmap()
             .load(url)
             .override(250,250)
-            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-            .dontAnimate()
             .apply(
                 RequestOptions()
+                    .signature(ObjectKey(getSignatureForImageLoading(item.date)))
+                    .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                     .placeholder(R.drawable.loading_animation)
                     .error(
                         CreateImageOverlay
@@ -68,6 +68,7 @@ fun setImageUrl(imageView: ImageView, item: AudioBookDomainModel?) {
                             .buildOverlay(front = R.drawable.ic_book_play,back = R.drawable.gradiant_background)
                     )
             )
+            .dontAnimate()
             .into(imageView)
     }
 }
@@ -109,60 +110,6 @@ fun TextView.setBookTitle(item: AudioBookDomainModel?){
             getNormalizedText(item.title, 30)
     }
 }
-
-private fun getNormalizedText(text:String?,limit:Int):String{
-    if(text?.length?:0>limit){
-        return text?.substring(0,limit-3)+"..."
-    }
-
-    return text?:""
-}
-
-private fun convertDateToTime(date:String?,context: Context) = date?.let {
-    calculateDateDiff(it, context)
-}?:"-"
-
-
-@Suppress("DEPRECATION", "RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-private fun calculateDateDiff(dateStr: String, context: Context?): String {
-
-    if (context == null) return "-"
-
-    val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'",
-        getCurrentLocale(context)
-    )
-    format.timeZone = TimeZone.getTimeZone("UTC")
-
-    try {
-
-        val date = format.parse(dateStr)
-        val diff = Date().time - date.time //this is going to give you the difference in milliseconds
-
-        val result = Date(diff)
-
-        return if (result.year - 70 > 0) {
-             "${result.year - 70}y"
-        } else if (result.month > 0) {
-             "${1 + result.month}m"
-        } else {
-            "${result.date}d"
-        }
-
-    } catch (e: Exception) {
-        Timber.e(e.printStackTrace().toString())
-        return "-"
-    }
-}
-
-@Suppress("DEPRECATION")
-private fun getCurrentLocale(context: Context): Locale {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        context.resources.configuration.locales.get(0)
-    } else {
-        context.resources.configuration.locale
-    }
-}
-
 
 @BindingAdapter("toolbarItemVisibility")
 fun setToolbarItemVisibility(view: View, showDisplaySearch: Boolean){
