2709fb4fb1a314943ce6095b554d321d198dddc0
Pravinyo <pravinyo12@gmail.com>
2020-05-17 18:38:29 +0530

    loading progress bug fixed

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/presentation/AudioBookDetailsFragment.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/presentation/AudioBookDetailsFragment.kt
@@ -271,7 +271,7 @@ class AudioBookDetailsFragment : BaseUIFragment(),KoinComponent {
 
     private fun removeLoading() {
         setVisibility(dataBindingReference.networkNoConnection,set = false)
-//        setVisibility(dataBindingReference.pbContentLoading,set = false)
+        setVisibility(dataBindingReference.pbContentLoading,set = false)
     }
 
     private fun handleDownloaderEvent(event: Event<DownloadEvent>) {
