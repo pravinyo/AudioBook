3f66d97665ad15dd236e5d726d2d213f440b5916
Pravinyo <pravinyo12@gmail.com>
2020-05-19 22:19:27 +0530

    implementation added for get all books

--- feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/data/repository/LocalBooksRepositoryImpl.kt
+++ feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/data/repository/LocalBooksRepositoryImpl.kt
@@ -1,21 +1,55 @@
 package com.allsoftdroid.audiobook.feature_mybooks.data.repository
 
-import com.allsoftdroid.audiobook.feature_mybooks.data.model.LocalBookDomainModel
+import android.app.Application
+import android.os.Environment
+import com.allsoftdroid.audiobook.feature_mybooks.data.model.LocalBookFiles
 import com.allsoftdroid.audiobook.feature_mybooks.domain.ILocalBooksRepository
+import com.allsoftdroid.common.base.network.ArchiveUtils
+import com.allsoftdroid.common.base.utils.LocalFilesForBook
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 
-class LocalBooksRepositoryImpl : ILocalBooksRepository {
+class LocalBooksRepositoryImpl(
+    private val dispatcher: CoroutineDispatcher=Dispatchers.IO,
+    private val application: Application
+) : ILocalBooksRepository {
 
-    private val list:List<LocalBookDomainModel> = emptyList()
+    override suspend fun getLocalBookFiles(): List<LocalBookFiles> =
+        withContext(dispatcher){
+            val localBookFiles = mutableListOf<LocalBookFiles>()
 
-    override suspend fun getAllBooks(): List<LocalBookDomainModel> {
-        return list
-    }
+            val rootFolder = ArchiveUtils.getDownloadsRootFolder(application)
+            Timber.d("Root Folder is $rootFolder")
+
+            val directory = Environment.getExternalStoragePublicDirectory("$rootFolder/AudioBooks/")
+            val bookIds = directory.listFiles()?.map {
+                it.absolutePath.split("/").last()
+            }
+
+            Timber.d("BookIds are  $bookIds")
+
+            val localFilesForBook = LocalFilesForBook(application)
+
+            bookIds?.map {bookId->
+                val files = localFilesForBook.getDownloadedFilesList(bookId)
+                if(files.isNullOrEmpty()){
+                    localBookFiles.add(LocalBookFiles(identifier = bookId,filePath = emptyList()))
+                }else{
+                    Timber.i("$bookId : Found ${files.size} files")
+                    localBookFiles.add(LocalBookFiles(identifier = bookId,filePath = files))
+                }
+            }
+
+            return@withContext localBookFiles
+        }
 
     override suspend fun removeBook(identifier: String) {
-
+        //delete sub-directory
     }
 
     override suspend fun deleteAllChapters(identifier: String) {
-
+        //delete all the files in the sub-directory
     }
 }
