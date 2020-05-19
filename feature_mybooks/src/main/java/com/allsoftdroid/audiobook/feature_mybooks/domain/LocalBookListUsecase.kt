d901ed7226815c8ce8c35ef1440e57cd7d5f4dff
Pravinyo <pravinyo12@gmail.com>
2020-05-19 23:55:03 +0530

    comments removed

--- feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/domain/LocalBookListUsecase.kt
+++ feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/domain/LocalBookListUsecase.kt
@@ -9,9 +9,7 @@ class LocalBookListUsecase(
 ) {
 
     suspend fun getBookList():List<LocalBookDomainModel>{
-        // Read all the sub-directory of the folder and use it as id to get the books details from DB
-        // If Book id matches get the details and return the list else ignore the bookId
-        //Use localBookForFiles to get the content in the file and missing
+
         val localBooks = mutableListOf<LocalBookDomainModel>()
 
         val localFiles = localBooksRepository.getLocalBookFiles()
@@ -20,6 +18,8 @@ class LocalBookListUsecase(
         for (file in localFiles){
 
             val metadata = bookMetadataRepository.getBookMetadata(file.identifier)
+
+            Timber.d("File Identifier is : ${file.identifier}")
             Timber.d("Metadata is : ${metadata.title}")
             Timber.d("File is : ${file.filePath}")
 
