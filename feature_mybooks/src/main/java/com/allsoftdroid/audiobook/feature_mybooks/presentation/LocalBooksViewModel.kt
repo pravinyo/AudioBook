a7e2113d2a085835fb6691284333e82ab4089080
Pravinyo <pravinyo12@gmail.com>
2020-05-19 23:02:50 +0530

    load book list implemented

--- feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/presentation/LocalBooksViewModel.kt
+++ feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/presentation/LocalBooksViewModel.kt
@@ -5,16 +5,22 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.allsoftdroid.audiobook.feature_mybooks.di.LocalBooksModule.SUPER_VISOR_JOB
 import com.allsoftdroid.audiobook.feature_mybooks.di.LocalBooksModule.VIEW_MODEL_SCOPE
+import com.allsoftdroid.audiobook.feature_mybooks.domain.LocalBookListUsecase
 import com.allsoftdroid.audiobook.feature_mybooks.utils.Empty
 import com.allsoftdroid.audiobook.feature_mybooks.utils.RequestStatus
+import com.allsoftdroid.audiobook.feature_mybooks.utils.Started
+import com.allsoftdroid.audiobook.feature_mybooks.utils.Success
 import com.allsoftdroid.common.base.extension.Event
 import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import org.koin.core.qualifier.named
 
-class LocalBooksViewModel : ViewModel(),KoinComponent {
+class LocalBooksViewModel(
+    private val bookListUsecase: LocalBookListUsecase
+) : ViewModel(),KoinComponent {
     /**
      * cancelling this job cancels all the job started by this viewmodel
      */
@@ -29,9 +35,21 @@ class LocalBooksViewModel : ViewModel(),KoinComponent {
     val requestStatus : LiveData<Event<RequestStatus>> = _requestStatus
 
     init {
-        _requestStatus.value = Event(Empty)
+        loadBooks()
     }
 
+    fun loadBooks(){
+        viewModelScope.launch {
+            _requestStatus.value = Event(Started)
+
+            val books = bookListUsecase.getBookList()
+            if(books.isEmpty()){
+                _requestStatus.value = Event(Empty)
+            }else{
+                _requestStatus.value = Event(Success(books))
+            }
+        }
+    }
 
     //cancel the job when viewmodel is not longer in use
     override fun onCleared() {
