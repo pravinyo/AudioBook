7c4eea673a23f707ea2efc1816061c62008bd8ec
Pravinyo <pravinyo12@gmail.com>
2020-05-20 12:00:28 +0530

    load from memory cache if available

--- feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/presentation/LocalBooksViewModel.kt
+++ feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/presentation/LocalBooksViewModel.kt
@@ -3,6 +3,7 @@ package com.allsoftdroid.audiobook.feature_mybooks.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.allsoftdroid.audiobook.feature_mybooks.data.model.LocalBookDomainModel
 import com.allsoftdroid.audiobook.feature_mybooks.di.LocalBooksModule.SUPER_VISOR_JOB
 import com.allsoftdroid.audiobook.feature_mybooks.di.LocalBooksModule.VIEW_MODEL_SCOPE
 import com.allsoftdroid.audiobook.feature_mybooks.domain.LocalBookListUsecase
@@ -32,14 +33,13 @@ class LocalBooksViewModel(
      */
     private val viewModelScope : CoroutineScope by inject(named(name = VIEW_MODEL_SCOPE))
 
+    private var _books:List<LocalBookDomainModel>?  = null
+
     private var _requestStatus = MutableLiveData<Event<RequestStatus>>()
     val requestStatus : LiveData<Event<RequestStatus>> = _requestStatus
 
-    init {
-        loadBooks()
-    }
 
-    fun loadBooks(){
+    private fun loadBooks(){
         viewModelScope.launch {
             Timber.d("sending started response")
             _requestStatus.value = Event(Started)
@@ -51,7 +51,19 @@ class LocalBooksViewModel(
             }else{
                 Timber.d("books is not empty sending response")
                 _requestStatus.value = Event(Success(books))
+                _books = books
             }
+        }
+    }
+
+    fun loadFromCacheOrReload(){
+
+        val books = _books
+
+        if (books.isNullOrEmpty()){
+            loadBooks()
+        }else{
+            _requestStatus.value = Event(Success(books))
         }
     }
 
