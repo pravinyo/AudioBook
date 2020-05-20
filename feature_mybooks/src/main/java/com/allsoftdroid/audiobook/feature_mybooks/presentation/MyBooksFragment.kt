e1bf0f23aa6840aaf2c6f9437dde542fb65e522d
Pravinyo <pravinyo12@gmail.com>
2020-05-20 12:00:59 +0530

    load data in resume

--- feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/presentation/MyBooksFragment.kt
+++ feature_mybooks/src/main/java/com/allsoftdroid/audiobook/feature_mybooks/presentation/MyBooksFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.allsoftdroid.audiobook.feature_mybooks.R
 import com.allsoftdroid.audiobook.feature_mybooks.databinding.FragmentMybooksLayoutBinding
@@ -41,8 +43,12 @@ class MyBooksFragment : BaseUIFragment(),KoinComponent {
         val adapter = LocalBookAdapter(
             this.requireActivity(),
 
-            ItemClickedListener {
-                Toast.makeText(this.requireActivity(), it,Toast.LENGTH_SHORT).show()
+            ItemClickedListener {bookId->
+                //Navigate to display page
+                val bundle = bundleOf("bookId" to bookId)
+
+                this.findNavController()
+                    .navigate(R.id.action_MyBooksFragment_to_AudioBookDetailsFragment,bundle)
             },
 
             OptionsClickedListener(
@@ -109,5 +115,10 @@ class MyBooksFragment : BaseUIFragment(),KoinComponent {
     override fun handleBackPressEvent(callback: OnBackPressedCallback) {
         callback.isEnabled = false
         requireActivity().onBackPressed()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        localBooksViewModel.loadFromCacheOrReload()
     }
 }
