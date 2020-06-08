cd1a0ffeb9cdeb4e12ee9e5d35de7cb197250c4e
Pravinyo <pravinyo12@gmail.com>
2020-06-08 16:36:35 +0530

    title disappear fixed

--- feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/utils/BindingUtil.kt
+++ feature_book_details/src/main/java/com/allsoftdroid/feature/book_details/utils/BindingUtil.kt
@@ -134,7 +134,7 @@ fun setBookBanner(layout: ConstraintLayout, item: AudioBookMetadataDomainModel?)
 
                         paletteBuilder.generate{
                             it?.let {
-                                val dark = it.getDarkMutedColor(it.getMutedColor(0))
+                                val dark = it.getDarkMutedColor(it.getDarkVibrantColor(0))
                                 val dominant = it.getDominantColor(it.getVibrantColor(0))
                                 val light = it.getLightMutedColor(it.getLightVibrantColor(0))
 
@@ -143,8 +143,10 @@ fun setBookBanner(layout: ConstraintLayout, item: AudioBookMetadataDomainModel?)
                                 with(layout.rootView.findViewById<View>(R.id.toolbar)){
                                     setBackgroundColor(dark)
 
-                                    this.findViewById<TextView>(R.id.tv_toolbar_title).apply {
-                                        this.setTextColor(light)
+                                    if(dark!=light){
+                                        this.findViewById<TextView>(R.id.tv_toolbar_title).apply {
+                                            this.setTextColor(light)
+                                        }
                                     }
                                 }
                             }
