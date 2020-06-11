fecf6060c20cdb680aa490b6cc4ccb2b4a8464aa
Pravinyo <pravinyo12@gmail.com>
2020-06-11 14:32:19 +0530

    return type changed to byte

--- feature_downloader/src/main/java/com/allsoftdroid/audiobook/feature_downloader/domain/IDownloader.java
+++ feature_downloader/src/main/java/com/allsoftdroid/audiobook/feature_downloader/domain/IDownloader.java
@@ -22,7 +22,7 @@ public interface IDownloader extends IDownloaderCore {
     String findURLbyDownloadId(long downloadId);
     long getDownloadIdByURL(String url);
 
-    String[] getStatusByDownloadId(long downloadId);
+    byte getStatusByDownloadId(long downloadId);
 
     void openDownloadedFile(Context context, long downloadId);
 }
