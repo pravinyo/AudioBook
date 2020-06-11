73bc22249fda57daa2945b33343ed29a663e353c
Pravinyo <pravinyo12@gmail.com>
2020-06-11 21:51:40 +0530

    getter for root downloader folder modified

--- common/src/main/java/com/allsoftdroid/common/base/network/ArchiveUtils.kt
+++ common/src/main/java/com/allsoftdroid/common/base/network/ArchiveUtils.kt
@@ -2,15 +2,17 @@ package com.allsoftdroid.common.base.network
 
 import android.app.Application
 import android.content.Context
-import android.os.Environment
+import androidx.core.content.ContextCompat
 import com.allsoftdroid.common.R
+import timber.log.Timber
+import java.util.*
 
 class ArchiveUtils {
     companion object{
 
         private const val BASE_DOWNLOAD_URL = "https://archive.org/download"
         private const val BASE_IMAGE_URL = "https://archive.org/services/img"
-        const val AppFolderName = "AudioBooks"
+        const val AppFolderName = "AudioBooksApp"
 
         /**
          * This function generated the file path on the remote server
@@ -34,10 +36,33 @@ class ArchiveUtils {
         }
 
         fun getDownloadsRootFolder(context: Application): String {
-            val default = Environment.DIRECTORY_DOWNLOADS
+            val default = ""
             val sharedPref = context.getSharedPreferences(context.getString(R.string.downloads_path),Context.MODE_PRIVATE)
 
-            return sharedPref.getString(context.getString(R.string.downloads_root_directory_key),default)?:default
+            val rootFolder =  sharedPref.getString(context.getString(R.string.downloads_root_directory_key),default)
+
+            if(rootFolder.isNullOrEmpty()){
+
+                val dir2 = ContextCompat.getExternalFilesDirs(context, null)
+
+                val dir: MutableList<String> = ArrayList()
+
+                val packageName: String = context.packageName
+
+                for (file in dir2) {
+                    Timber.d("external file dir is : %s", file.absolutePath)
+                    dir.add(
+                        file.absolutePath
+                            .replace("/Android/data/$packageName/files", "")
+                    )
+                }
+
+                setDownloadsRootFolder(context,dir[0])
+
+                return dir[0]
+            }
+
+            return rootFolder
         }
     }
 }
