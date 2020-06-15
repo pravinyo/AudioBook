f9533fffbbe45d88bdaada127a458ba00c248acb
Pravinyo <pravinyo12@gmail.com>
2020-06-15 13:51:52 +0530

    path 3 applied, version incremented

--- buildSrc/src/main/java/com/allsoftdroid/buildsrc/AndroidConfig.kt
+++ buildSrc/src/main/java/com/allsoftdroid/buildsrc/AndroidConfig.kt
@@ -8,7 +8,7 @@ object AndroidConfig {
 
     const val versionMajor = 6
     const val versionMinor = 5
-    const val versionPatch = 2
+    const val versionPatch = 3
     var versionClassifier = "SNAPSHOT"
     const val isSnapshot = false
 
