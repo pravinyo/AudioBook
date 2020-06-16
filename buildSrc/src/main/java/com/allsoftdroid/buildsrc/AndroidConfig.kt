4f1c31064ffe10c03991e38a1756c4991b43ad28
Pravinyo <pravinyo12@gmail.com>
2020-06-16 13:40:42 +0530

    version incremented

--- buildSrc/src/main/java/com/allsoftdroid/buildsrc/AndroidConfig.kt
+++ buildSrc/src/main/java/com/allsoftdroid/buildsrc/AndroidConfig.kt
@@ -8,7 +8,7 @@ object AndroidConfig {
 
     const val versionMajor = 6
     const val versionMinor = 5
-    const val versionPatch = 4
+    const val versionPatch = 5
     var versionClassifier = "SNAPSHOT"
     const val isSnapshot = false
 
