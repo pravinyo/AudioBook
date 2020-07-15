9a0e17854ce45a65ba982664f010655d2e299423
Pravinyo <pravinyo12@gmail.com>
2020-07-15 16:23:39 +0530

    browser library dependency added

--- buildSrc/src/main/java/com/allsoftdroid/buildsrc/LibraryDependency.kt
+++ buildSrc/src/main/java/com/allsoftdroid/buildsrc/LibraryDependency.kt
@@ -3,6 +3,7 @@ package com.allsoftdroid.buildsrc
 import CoreVersion
 
 private object LibraryVersion {
+    const val BROWSER_VERSION = "1.2.0"
     const val DOWNLOADER_VERSION = "1.7.7"
     const val TOOL_TIP = "2.0.3"
     const val FIREBASE_CRASHLYTICS = "17.0.0"
@@ -130,4 +131,6 @@ object LibraryDependency {
 
     const val TOOL_TIP = "it.sephiroth.android.library.targettooltip:target-tooltip-library:${LibraryVersion.TOOL_TIP}"
     const val DOWNLOADER = "com.liulishuo.filedownloader:library:${LibraryVersion.DOWNLOADER_VERSION}"
+
+    const val BROWSER = "androidx.browser:browser:${LibraryVersion.BROWSER_VERSION}"
 }
