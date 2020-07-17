36a859cda6987cab8d8dda584f24f90d36f71b96
Pravinyo <pravinyo12@gmail.com>
2020-07-17 21:52:40 +0530

    livedata replaced with Flow

--- feature_book/src/main/java/com/allsoftdroid/feature_book/domain/repository/AudioBookRepository.kt
+++ feature_book/src/main/java/com/allsoftdroid/feature_book/domain/repository/AudioBookRepository.kt
@@ -1,15 +1,15 @@
 package com.allsoftdroid.feature_book.domain.repository
 
-import androidx.lifecycle.LiveData
 import com.allsoftdroid.feature_book.domain.model.AudioBookDomainModel
+import kotlinx.coroutines.flow.Flow
 
 interface AudioBookRepository {
 
     suspend fun fetchBookList(page : Int)
-    fun getAudioBooks() : LiveData<List<AudioBookDomainModel>>
+    fun getAudioBooks() : Flow<List<AudioBookDomainModel>>
 
     suspend fun searchBookList(query:String,page: Int)
-    fun getSearchBooks() : LiveData<List<AudioBookDomainModel>>
+    fun getSearchBooks() : Flow<List<AudioBookDomainModel>>
 
     fun registerNetworkResponse(listener: NetworkResponseListener)
     fun unRegisterNetworkResponse()
