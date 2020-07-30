150cb4ec623307ca8f643cb4731da6c897f92eaa
Pravinyo <pravinyo12@gmail.com>
2020-07-30 11:57:52 +0530

    usecase implemented for export user data

--- feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/domain/usecase/ExportUserData.kt
+++ feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/domain/usecase/ExportUserData.kt
@@ -2,18 +2,17 @@ package com.allsoftdroid.audiobook.feature_listen_later_ui.domain.usecase
 
 import android.os.ParcelFileDescriptor
 import com.allsoftdroid.audiobook.feature_listen_later_ui.data.model.toExternalModel
-import com.allsoftdroid.audiobook.feature_listen_later_ui.data.repository.ExportUserDataRepository
+import com.allsoftdroid.audiobook.feature_listen_later_ui.domain.repository.IExportUserDataRepository
 import com.allsoftdroid.database.listenLaterDB.ListenLaterDao
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 import timber.log.Timber
-import java.lang.Exception
 
 class ExportUserData(
     private val listenLaterDao: ListenLaterDao,
-    private val exportUserDataRepository: ExportUserDataRepository) {
+    private val exportUserDataRepository: IExportUserDataRepository) {
 
     suspend fun exportToPath(path:String):Flow<Boolean>{
         var result:Flow<Boolean> = flow {}
