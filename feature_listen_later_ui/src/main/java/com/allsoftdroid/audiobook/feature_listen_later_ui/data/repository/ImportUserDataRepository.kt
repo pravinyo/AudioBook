dc8cdbcb3c5105dba1cdd4d5f7a0792eb3da4d71
Pravinyo <pravinyo12@gmail.com>
2020-07-30 11:57:19 +0530

    code cleaned

--- feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/data/repository/ImportUserDataRepository.kt
+++ feature_listen_later_ui/src/main/java/com/allsoftdroid/audiobook/feature_listen_later_ui/data/repository/ImportUserDataRepository.kt
@@ -17,7 +17,29 @@ import java.lang.Exception
 class ImportUserDataRepository(private val dispatcher:CoroutineDispatcher = Dispatchers.IO) :
     IImportUserDataRepository {
 
-    private val secretKey: String = "662ede816988e58fb6d057d9d85605e0"
+    private fun getParsedList(jsonString:String?):List<BookMarkDataItem> {
+        return if (jsonString == null) emptyList()
+        else{
+            val returnList = mutableListOf<BookMarkDataItem>()
+            val jsonArray = JSONArray(jsonString)
+            Timber.d("json array is $jsonArray")
+            Timber.d("json array length is ${jsonArray.length()}")
+
+            for (i in 0 until jsonArray.length()){
+                val item = jsonArray[i] as JSONObject
+                returnList.add(
+                    BookMarkDataItem(
+                        bookId = item.getString("bookId"),
+                        bookName = item.getString("bookName"),
+                        bookAuthor = item.getString("bookAuthor"),
+                        duration = item.getString("duration"),
+                        timeStamp = item.getString("timeStamp")
+                    ))
+            }
+            Timber.d("return list is $returnList")
+            returnList
+        }
+    }
 
     override suspend fun fromFile(path: String): List<BookMarkDataItem> {
         val jsonString:String? = withContext(dispatcher) {
@@ -25,66 +47,25 @@ class ImportUserDataRepository(private val dispatcher:CoroutineDispatcher = Disp
         }
 
         Timber.d("Json string received is : $jsonString")
-
-        return if (jsonString == null) emptyList()
-        else{
-            val returnList = mutableListOf<BookMarkDataItem>()
-            val jsonArray = JSONArray(jsonString)
-            Timber.d("json array is $jsonArray")
-            Timber.d("json array length is ${jsonArray.length()}")
-
-            for (i in 0 until jsonArray.length()){
-                val item = jsonArray[i] as JSONObject
-                returnList.add(
-                    BookMarkDataItem(
-                        bookId = item.getString("bookId"),
-                        bookName = item.getString("bookName"),
-                        bookAuthor = item.getString("bookAuthor"),
-                        duration = item.getString("duration"),
-                        timeStamp = item.getString("timeStamp")
-                    ))
-            }
-            Timber.d("return list is $returnList")
-            returnList
-        }
+        return getParsedList(jsonString)
     }
 
-    suspend fun fromFile(pfd: ParcelFileDescriptor): List<BookMarkDataItem> {
+    override suspend fun fromFile(pfd: ParcelFileDescriptor): List<BookMarkDataItem> {
         val jsonString:String? = withContext(dispatcher) {
             getContent(pfd)
         }
 
         Timber.d("Json string received is : $jsonString")
 
-        return if (jsonString == null) emptyList()
-        else{
-            val returnList = mutableListOf<BookMarkDataItem>()
-            val jsonArray = JSONArray(jsonString)
-            Timber.d("json array is $jsonArray")
-            Timber.d("json array length is ${jsonArray.length()}")
-
-            for (i in 0 until jsonArray.length()){
-                val item = jsonArray[i] as JSONObject
-                returnList.add(
-                    BookMarkDataItem(
-                        bookId = item.getString("bookId"),
-                        bookName = item.getString("bookName"),
-                        bookAuthor = item.getString("bookAuthor"),
-                        duration = item.getString("duration"),
-                        timeStamp = item.getString("timeStamp")
-                    ))
-            }
-            Timber.d("return list is $returnList")
-            returnList
-        }
+        return getParsedList(jsonString)
     }
 
     private fun getContent(pfd: ParcelFileDescriptor):String?{
-        var result:String?=null
+        val result: String?
 
         val fileStream = FileInputStream(pfd.fileDescriptor)
         val encryptedData = fileStream.bufferedReader().use { input -> input.readText() }
-        result = CommonUtility.decryptWithAES(secretKey,encryptedData)
+        result = CommonUtility.decryptWithAES(CommonUtility.defaultSecretKey,encryptedData)
 
         return result
     }
@@ -103,7 +84,7 @@ class ImportUserDataRepository(private val dispatcher:CoroutineDispatcher = Disp
                     Timber.d("File is ${fileList.first().name}")
                     val encryptedData = File(path,fileList.first().name).bufferedReader().use { input -> input.readText() }
                     Timber.d("Encrypted data is :$encryptedData")
-                    result = CommonUtility.decryptWithAES(secretKey,encryptedData)
+                    result = CommonUtility.decryptWithAES(CommonUtility.defaultSecretKey,encryptedData)
                     if(result==null){
                         Timber.d("Decryption failed")
                     }
